8d590cce4d59371022f24054dda948b82097f580
dpage <dpage>
2008-09-05 09:44:34 +0000

    More robust checks for a way to elevate privileges

--- server/scripts/linux/launchstackbuilder.sh
+++ server/scripts/linux/launchstackbuilder.sh
@@ -3,13 +3,42 @@
 # PostgreSQL stackbuilder launch script for Linux
 # Dave Page, EnterpriseDB
 
-which gksu 1> /dev/null 2> /dev/null
-if [ $? -eq 0 ];
+CMD=""
+
+# KDESU
+if [ x"$KDE_FULL_SESSION" = x"true" ];
 then
-    GKSU=`which gksu`
-    GKSU="$GKSU -u root -D StackBuilder"
+    KDESU=`which kdesu 2> /dev/null`
+    if [ $? -eq 0 ];
+    then
+        CMD="$KDESU -c"
+    fi
+
+# GNOMESU
+elif [ x"$GNOME_DESKTOP_SESSION_ID" != x"" ];
+then
+    GNOMESU=`which gnomesu 2> /dev/null`
+    if [ $? -ne 0 ];
+    then
+        GNOMESU=`which xsu 2> /dev/null`
+    fi
+
+    if [ $? -eq 0 ];
+    then
+        CMD="$GNOMESU -c"
+    fi
 fi
 
-$GKSU "PG_INSTALLDIR/scripts/runstackbuilder.sh"
+# If we still have nothing, look for gksu
+if [ x"$CMD" = x"" ];
+then
+    GKSU=`which gksu 2> /dev/null`
+    if [ $? -eq 0 ];
+    then
+        CMD="$GKSU -u root -D StackBuilder"
+    fi
+fi
+
+$CMD "PG_INSTALLDIR/scripts/runstackbuilder.sh"
 
 
