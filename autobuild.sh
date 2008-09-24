bb7a896937857169e779866ebe35f00134b4d429
dpage <dpage>
2008-09-24 10:52:36 +0000

    Add the auto build script

---
+++ autobuild.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# pgInstaller auto build script
+# Dave Page, EnterpriseDB
+
+if [ $# -ne 1 ]; 
+then
+    echo "Usage: $0 <build dir>"
+    exit 127
+fi
+
+# Run everything from the root of the buld directory
+cd $1
+
+echo "#######################################################################" >> autobuild.log
+echo "Build run starting at `date`" >> autobuild.log
+echo "#######################################################################" >> autobuild.log
+
+# Clear out any old output
+echo "Cleaning up old output" >> autobuild.log
+rm -rf output/* >> autobuild.log
+
+# Self update
+echo "Updating build system" >> autobuild.log
+cvs update -dP >> autobuild.log
+
+# Run the build, and dump the output to a log file
+echo "Running the build" >> autobuild.log
+./build.sh > output/build.log
+
+# Create a remote directory and upload the output.
+DATE=`date +'%Y-%m-%d'`
+echo "Creating pub/pgInstaller/$DATE on the sftp server" >> autobuild.log
+ssh dave.page@sftp.enterprisedb.com mkdir -p pub/pgInstaller/$DATE
+
+echo "Uploading output to pub/pgInstaller/$DATE on the sftp server" >> autobuild.log
+scp output/* dave.page@sftp.enterprisedb.com:pub/pgInstaller/$DATE
+
+echo "#######################################################################" >> autobuild.log
+echo "Build run completed at `date`" >> autobuild.log
+echo "#######################################################################" >> autobuild.log
+echo "" >> autobuild.log
