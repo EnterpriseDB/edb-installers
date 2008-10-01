595ee6a7c01dcdd64ddcb8d4cc7d886ddda2d672
asheshv <asheshv>
2008-10-01 16:05:11 +0000

    Added scripts for the support of linux-x64 for phpBB package

---
+++ phpBB/scripts/linux-x64/createshortcuts.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Check the command line
+if [ $# -ne 1 ]; 
+then
+    echo "Usage: $0 <Install dir> "
+    exit 127
+fi
+
+INSTALLDIR=$1
+
+# Exit code
+WARN=0
+
+# Working directory
+WD=`pwd`
+
+# Error handlers
+_die() {
+    echo $1
+    exit 1
+}
+
+_warn() {
+    echo $1
+    WARN=2
+}
+
+# Search & replace in a file - _replace($find, $replace, $file) 
+_replace() {
+    sed -e "s^$1^$2^g" $3 > "/tmp/$$.tmp" || _die "Failed for search and replace '$1' with '$2' in $3"
+    mv /tmp/$$.tmp $3 || _die "Failed to move /tmp/$$.tmp to $3"
+}
+
+# Substitute values into a file ($in)
+_fixup_file() {
+    _replace INSTALL_DIR $INSTALLDIR $1
+}
+
+# Create the icon resources
+cd "$INSTALLDIR/phpBB/scripts/images"
+for i in `ls *.png`
+do
+    "$INSTALLDIR/phpBB/installer/xdg/xdg-icon-resource" install --size 32 $i
+done
+
+# Fixup the scripts
+_fixup_file "$INSTALLDIR/phpBB/scripts/launchbrowser.sh"
+_fixup_file "$INSTALLDIR/phpBB/scripts/launchPhpBB.sh"
+
+chmod ugo+x "$INSTALLDIR/phpBB/scripts/"*.sh
+chmod ugo+x "$INSTALLDIR/phpBB/installer/phpBB/"*.sh
+
+# Fixup the XDG files (don't just loop in case we have old entries we no longer want)
+_fixup_file "$INSTALLDIR/phpBB/scripts/xdg/enterprisedb-postgres.directory"
+_fixup_file "$INSTALLDIR/phpBB/scripts/xdg/enterprisedb-launchPhpBB.desktop"
+
+# Create the menu shortcuts - first the top level, then the documentation menu.
+"$INSTALLDIR/phpBB/installer/xdg/xdg-desktop-menu" install --mode system \
+      "$INSTALLDIR/phpBB/scripts/xdg/enterprisedb-postgres.directory" \
+      "$INSTALLDIR/phpBB/scripts/xdg/enterprisedb-launchPhpBB.desktop"  || _warn "Failed to create the phpBB menu"
+
+echo "$0 ran to completion"
+exit 0
---
+++ phpBB/scripts/linux-x64/removeshortcuts.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Check the command line
+if [ $# -ne 1 ]; 
+then
+    echo "Usage: $0 <Install dir>"
+    exit 127
+fi
+
+INSTALLDIR=$1
+
+# Exit code
+WARN=0
+
+# Working directory
+WD=`pwd`
+
+# Error handlers
+_die() {
+    echo $1
+    exit 1
+}
+
+_warn() {
+    echo $1
+    WARN=2
+}
+
+# Remove the menu shortcuts
+"$INSTALLDIR/phpBB/installer/xdg/xdg-desktop-menu" uninstall --mode system  \
+        "$INSTALLDIR/phpBB/scripts/xdg/enterprisedb-launchPhpBB.desktop" || _warn "Failed to remove the phpBB menu"
+
+# Remove the icon resources
+"$INSTALLDIR/phpBB/installer/xdg/xdg-icon-resource" uninstall --mode system --size 32 \
+    "$INSTALLDIR/phpBB/scripts/images/enterprisedb-launchPhpBB.png" ||  _warn "Failed to remove icon resource"
+
+echo "$0 ran to completion"
+exit 0
+
