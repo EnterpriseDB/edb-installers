86a7184f78f6531be8a77606d15f6043b52b7d58
dpage <dpage>
2008-10-27 14:19:34 +0000

    Fix tarball name

--- mediaWiki/build.sh
+++ mediaWiki/build.sh
@@ -49,7 +49,7 @@ _prep_mediaWiki() {
     fi
 
     echo "Unpacking MediaWiki source..."
-    extract_file ../../tarballs/mediawiki-$PG_VERSION_MEDIAWIKI.tar.bz2 || exit 1
+    extract_file ../../tarballs/mediawiki-$PG_VERSION_MEDIAWIKI.tar.gz || exit 1
 
     # Per-platform prep
     cd $WD
