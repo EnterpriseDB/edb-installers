ce5cb7e52068955db2edb9a675811c6221f3d975
ravinderp <ravinderp>
2008-11-05 09:13:00 +0000

    removed files from PostGIS/scripts/linux-x64 folder as those are no longer used

--- PostGIS/scripts/linux-x64/launchJDBCDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-INSTALLDIR="INSTALL_DIR"
-URL="file://$INSTALLDIR/doc/postgis/jdbc/index.html"
-
-"$INSTALLDIR/PostGIS/scripts/launchbrowser.sh" $URL
-
--- PostGIS/scripts/linux-x64/launchPostGISDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-INSTALLDIR="INSTALL_DIR"
-URL="file://$INSTALLDIR/doc/contrib/html/postgis/postgis.html"
-
-"$INSTALLDIR/PostGIS/scripts/launchbrowser.sh" $URL
-
