1d396e8496492ad239bbd5535036a22018a2873e
dpage <dpage>
2009-01-20 15:56:17 +0000

    Add support for branding

--- psqlODBC/scripts/linux/removeshortcuts.sh
+++ psqlODBC/scripts/linux/removeshortcuts.sh
@@ -4,13 +4,25 @@
 # Dave Page, EnterpriseDB
 
 # Check the command line
-if [ $# -ne 1 ]; 
+if [ $# -ne 2 ]; 
 then
-    echo "Usage: $0 <Install dir>"
+    echo "Usage: $0 <Install dir> <Branding>"
     exit 127
 fi
 
 INSTALLDIR=$1
+BRANDING=$2
+
+# Branding string, for the xdg filenames. If the branding is 'PostgreSQL',
+# Don't do anything to ensure we remain backwards compatible.
+if [ "x$BRANDING" = "xPostgreSQL" ];
+then
+    BRANDING_STR="postgresql-$PG_VERSION_STR"
+    BRANDED=0
+else
+    BRANDING_STR=`echo $BRANDING | sed 's/\./_/g' | sed 's/ /_/g'`
+	BRANDED=1
+fi
 
 # Exit code
 WARN=0
@@ -31,10 +43,17 @@ _warn() {
 
 # Remove the menu shortcuts
 "$INSTALLDIR/installer/xdg/xdg-desktop-menu" uninstall --mode system \
+      "$INSTALLDIR/scripts/xdg/pg-$BRANDING_STR.directory" \
       "$INSTALLDIR/scripts/xdg/pg-launchOdbcDocs.desktop"  || _warn "Failed to Remove the pgODBC menu"
       
 # Remove the icon resources
 "$INSTALLDIR/installer/xdg/xdg-icon-resource" uninstall --mode system --size 32 "$INSTALLDIR/scripts/images/launch-OdbcDocs.png"
+
+# Only remove the directory file if it's branded
+if [ $BRANDED -ne 0 ];
+then
+    rm "$INSTALLDIR/scripts/xdg/pg-$BRANDING_STR.directory"
+fi
 
 echo "$0 ran to completion"
 exit 0
