203083ab73f924936313b28d8de75c50d5ad09f0
sachins <sachins>
2009-01-29 16:14:17 +0000

    Made changes for solving branding issues

--- Slony/scripts/linux/removeshortcuts.sh
+++ Slony/scripts/linux/removeshortcuts.sh
@@ -44,9 +44,14 @@ _warn() {
     WARN=2
 }
 
+# Search & replace in a file - _replace($find, $replace, $file) 
+_replace() {
+    sed -e "s^$1^$2^g" $3 > "/tmp/$$.tmp" || _die "Failed for search and replace '$1' with '$2' in $3"
+        mv /tmp/$$.tmp $3 || _die "Failed to move /tmp/$$.tmp to $3"
+}
+
 # Remove the menu shortcuts
 "$INSTALLDIR/Slony/installer/xdg/xdg-desktop-menu" uninstall --mode system   \
-    "$INSTALLDIR/Slony/scripts/xdg/pg-$BRANDING_STR.directory" \
     "$INSTALLDIR/Slony/scripts/xdg/pg-launchSlonyDocs-$SLONY_VERSION_STR.desktop" || _warn "Failed to remove the Slony docs menu item"
       
 # Remove the icon resources
@@ -57,6 +62,21 @@ if [ $BRANDED -ne 0 ];
 then
     rm "$INSTALLDIR/Slony/scripts/xdg/pg-$BRANDING_STR.directory"
 fi
+xdg_dir_name=menus
+
+xdg_system_dirs="$XDG_CONFIG_DIRS"
+[ -n "$xdg_system_dirs" ] || xdg_system_dirs=/etc/xdg
+xdg_global_dir=
+for x in `echo $xdg_system_dirs | sed 's/:/ /g'` ; do
+   if [ -w $x/$xdg_dir_name ] ; then
+       xdg_global_dir="$x/$xdg_dir_name"
+       break
+   fi
+done
+xdg_global_dir="$xdg_global_dir/applications-merged"
+
+# Hack up the XDG menu files to make sure everything really does go.
+_replace "<Filename>pg-launchSlonyDocs-$SLONY_VERSION_STR.desktop</Filename>" "" "$xdg_global_dir/pg-$BRANDING_STR.menu"
 
 echo "$0 ran to completion"
 exit 0
