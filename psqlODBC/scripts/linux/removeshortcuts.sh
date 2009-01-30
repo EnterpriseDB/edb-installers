466113a7c3883a559e481ccba7eb5fc2f771d27e
sachins <sachins>
2009-01-30 13:23:03 +0000

    Modified the branding logic for psqlODBC, added modification of .menu files while uninstallation

--- psqlODBC/scripts/linux/removeshortcuts.sh
+++ psqlODBC/scripts/linux/removeshortcuts.sh
@@ -43,7 +43,6 @@ _warn() {
 
 # Remove the menu shortcuts
 "$INSTALLDIR/installer/xdg/xdg-desktop-menu" uninstall --mode system \
-      "$INSTALLDIR/scripts/xdg/pg-$BRANDING_STR.directory" \
       "$INSTALLDIR/scripts/xdg/pg-launchOdbcDocs.desktop"  || _warn "Failed to Remove the pgODBC menu"
       
 # Remove the icon resources
@@ -54,6 +53,22 @@ if [ $BRANDED -ne 0 ];
 then
     rm "$INSTALLDIR/scripts/xdg/pg-$BRANDING_STR.directory"
 fi
+
+xdg_dir_name=menus
+
+xdg_system_dirs="$XDG_CONFIG_DIRS"
+[ -n "$xdg_system_dirs" ] || xdg_system_dirs=/etc/xdg
+xdg_global_dir=
+for x in `echo $xdg_system_dirs | sed 's/:/ /g'` ; do
+   if [ -w $x/$xdg_dir_name ] ; then
+       xdg_global_dir="$x/$xdg_dir_name"
+       break
+   fi
+done
+xdg_global_dir="$xdg_global_dir/applications-merged"
+
+# Hack up the XDG menu files to make sure everything really does go.
+_replace "<Filename>pg-launchOdbcDocs.desktop</Filename>" "" "$xdg_global_dir/pg-$BRANDING_STR.menu"
 
 echo "$0 ran to completion"
 exit 0
