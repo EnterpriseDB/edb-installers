ef7dee1ae40773321602152987b09e5c38d4236a
Sachin Srivastava <sachin@localhost.localdomain>
2009-03-04 20:01:44 +0530

    Changes to support edb default database and PG Server independency, mistakenly left for getserverlocale.sh

--- phpBB/scripts/linux/getserverlocale.sh
+++ phpBB/scripts/linux/getserverlocale.sh
@@ -6,11 +6,10 @@ export PGHOST=$1
 export PGPORT=$2
 export PGUSER=$3
 export PGPASSWORD=$4
-PG_HOME=$5
+TEMPDIR=$5
+DB=$6
 
-PGDATABASE=postgres
-
-locale=`$PG_HOME/bin/psql -U $PGUSER -c "SHOW LC_CTYPE" | grep -v lc_ctype | grep -v row | grep -v '\-\-'`
+locale=`$TEMPDIR/psql -d $DB -U $PGUSER -c "SHOW LC_CTYPE" | grep -v lc_ctype | grep -v row | grep -v '\-\-'`
 echo $locale
 
 export PGPASSWORD=$OLD_PGPASSWORD
