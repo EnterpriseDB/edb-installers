1f1198f2ba5d5f0e866c575dd0ee485d622a6f64
Sachin Srivastava <sachin@localhost.localdomain>
2009-04-09 19:24:25 +0530

    Solved FB # 11808, database superuser without home directory

--- server/scripts/linux/createuser.sh
+++ server/scripts/linux/createuser.sh
@@ -26,8 +26,17 @@ fi
 # Create the user account if required
 if [ "x`cat /etc/passwd|cut -f1 -d':'|grep $1`" != "x" ];
 then
-    echo "User account '$1' already exists"
-    exit 0
+
+    HOME_DIR=`su $1 -c "echo \\\$HOME"`
+    if [ -e $HOME_DIR ]; then
+        echo "User account '$1' already exists"
+        exit 0
+    else
+        echo "User account '$1' already exists - fixing non-existent home directory to $2"
+        usermod -d "$2" $1
+        exit 0
+    fi
+   
 else
 	useradd -m -c "PostgreSQL" -d "$2" -g $1 $1 || _die $1
 fi
