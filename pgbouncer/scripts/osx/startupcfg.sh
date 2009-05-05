92a780d5b196cd653b314454fba8deabf3c54f0f
Sachin Srivastava <sachin@localhost.localdomain>
2009-05-05 18:44:23 +0530

    Modified pgbouncer to configure pgbouncer as service (Linux) and daemon (Mac), Although some work is required to make service run in Windows

---
+++ pgbouncer/scripts/osx/startupcfg.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# Check the command line
+if [ $# -ne 1 ]; 
+then
+echo "Usage: $0 <Install dir> "
+    exit 127
+fi
+
+INSTALL_DIR=$1
+
+# Error handlers
+_die() {
+    echo $1
+    exit 1
+}
+
+_warn() {
+    echo $1
+    WARN=2
+}
+
+# Configure database startup
+if [ ! -e /Library/LaunchDaemons ]; then
+    mkdir -p /Library/LaunchDaemons || _die "Failed to create directory for root daemons"
+fi
+
+# Write the plist file
+cat <<EOT > "/Library/LaunchDaemons/com.edb.launchd.pgbouncer.plist"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+        "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Disabled</key>
+	<false/>
+        <key>Label</key>
+        <string>com.edb.launchd.pgbouncer</string>
+        <key>ProgramArguments</key>
+        <array>
+                <string>$INSTALL_DIR/installer/pgbouncer/pgbouncerctl.sh</string>
+                <string>start</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+	<key>UserName</key>
+        <string>root</string>
+</dict>
+</plist>
+EOT
+
+# Fixup the permissions on the launchDaemon
+chown -R root:wheel "/Library/LaunchDaemons/com.edb.launchd.pgbouncer.plist" || _warn "Failed to set the ownership of the launchd daemon for pgbouncer (/Library/LaunchDaemons/com.edb.launchd.pgbouncer.plist)"
+
+# Startup pgbouncer
+$INSTALL_DIR/installer/pgbouncer/pgbouncerctl.sh start
+
+echo "$0 ran to completion"
+exit $WARN
