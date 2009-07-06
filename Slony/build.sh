8dbdf0619e14301aeb6a0a07e116514725f07450
Sachin Srivastava <sachin@localhost.localdomain>
2009-07-06 13:02:24 +0530

    Removed the Patch we add for Slony to build with 8.4 as Slony 2.0.2v already has that

--- Slony/build.sh
+++ Slony/build.sh
@@ -62,9 +62,6 @@ _prep_Slony() {
     echo "Unpacking SLONY source..."
     extract_file  $WD/tarballs/slony1-$PG_VERSION_SLONY || exit 1
 
-    cd slony1-$PG_VERSION_SLONY
-    patch -p0 < $WD/tarballs/slony-postgresql-8.4.patch  
-
     # Per-platform prep
     cd $WD
     
