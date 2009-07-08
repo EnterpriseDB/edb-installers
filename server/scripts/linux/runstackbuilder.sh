3651cc7350303674d6fdfc2e9ce60cf0fb03d266
Ashesh D Vashi <ashesh@ashesh.edb.in>
2009-07-08 13:20:49 +0530

    The terminal window should not be blank, when we use the runstackbuilder.sh script from terminal as a root user.

--- server/scripts/linux/runstackbuilder.sh
+++ server/scripts/linux/runstackbuilder.sh
@@ -3,12 +3,12 @@
 # PostgreSQL stackbuilder runner script for Linux
 # Dave Page, EnterpriseDB
 LOADINGUSER=`whoami`
+echo "NOTE: Graphical administrator tool for su/sudo could not be located on your system! This window must be kept open, while the Stack Builder is running."
 
 # You're not running this script as root user
 if [ x"$LOADINGUSER" != x"root" ];
 then
 
-    echo "NOTE: Graphical administrator tool for su/sudo could not be located on your system! This window must be kept open, while the Stack Builder is running."
     USE_SUDO=0
 
     if [ -f /etc/lsb-release ];
