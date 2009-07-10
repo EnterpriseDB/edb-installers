3f9c4563220e3574103351fe2b656d01f615cf49
Sachin Srivastava <sachin@localhost.localdomain>
2009-07-10 21:19:20 +0530

    Fixed issue in windows 64-bit arising due to passing arguments with double quotes for drupal, mediawiki, phpBB, phpWiki

--- mediaWiki/scripts/windows/install.bat
+++ mediaWiki/scripts/windows/install.bat
@@ -1,24 +1,11 @@
 @ECHO OFF
 
-SET OLD_PGPASSWORD=%PGPASSWORD%
-SET PGHOST=%1
-SET PGPORT=%2
-SET PGUSER=%3
-SET PGPASSWORD=%4
-SET TEMPDIR="%5"
-SET DB=%6
-
-IF "%PGUSER%" == "" SET PGUSER=%USERNAME%
-IF "%PGPORT%" == "" SET PGPORT=5432
-
+SET TEMPDIR="%1"
+SET DB=%2
 
 REM Creating user for sample application
-"%TEMPDIR%\psql.exe" -d %DB% -U %PGUSER% -c "CREATE ROLE mediawikiuser PASSWORD 'mediawikiuser' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN"
+"%TEMPDIR%\psql.exe" -d %DB% -c "CREATE ROLE mediawikiuser PASSWORD 'mediawikiuser' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN"
 
 REM Creating database for sample application
-"%TEMPDIR%\psql.exe" -d %DB% -U %PGUSER% -c "CREATE DATABASE mediawiki OWNER mediawikiuser"
+"%TEMPDIR%\psql.exe" -d %DB% -c "CREATE DATABASE mediawiki OWNER mediawikiuser"
 
-
-:end
-
-SET PGPASSWORD=%OLD_PGPASSWORD%
--- phpWiki/scripts/windows/install.bat
+++ phpWiki/scripts/windows/install.bat
@@ -1,22 +1,16 @@
 @ECHO off
-SET OLD_PGPASSWORD=%PGPASSWORD%
 
-SET PGHOST=%1
-SET PGPORT=%2
-SET PGUSER=%3
-SET PGPASSWORD=%4
-SET TEMPDIR="%5"
-SET INSTALL_DIR="%6"
-
-SET DB=%7
+SET TEMPDIR="%1"
+SET INSTALL_DIR="%2"
+SET DB=%3
 
 REM Creating user for wiki application
-"%TEMPDIR%\psql.exe" -d %DB% -U %PGUSER% -c "CREATE ROLE phpwikiuser PASSWORD 'phpwikiuser' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN"
+"%TEMPDIR%\psql.exe" -d %DB% -c "CREATE ROLE phpwikiuser PASSWORD 'phpwikiuser' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN"
 
 REM Creating database for wiki application
-"%TEMPDIR%\psql.exe" -d %DB% -U %PGUSER% -c "CREATE DATABASE phpwiki OWNER phpwikiuser"
+"%TEMPDIR%\psql.exe" -d %DB% -c "CREATE DATABASE phpwiki OWNER phpwikiuser"
 
-CD %INSTALL_DIR%
+CD /d %INSTALL_DIR%
 IF EXIST phpwiki\wiki.sql GOTO install_schema
 ECHO phpwiki\wiki.sql is missing ..."
 goto end
@@ -26,4 +20,3 @@ goto end
     CALL "%TEMPDIR%\psql.exe" -U phpwikiuser -d phpwiki -f phpwiki\wiki.sql
 
 :end
-    SET PGPASSWORD=%OLD_PGPASSWORD%
