aefdfe8b2affc994ab3c96dcd404144bc2db33bf
IsbPGMac buildfarm <usman.saleem@enterprisedb.com>
2009-08-31 15:58:56 +0500

    Fix loadmodules bug. FB 12897

--- server/scripts/osx/loadmodules.sh
+++ server/scripts/osx/loadmodules.sh
@@ -4,17 +4,17 @@
 # Dave Page, EnterpriseDB
 
 #Check the command line
-if [ $# -ne 5 ]; 
+if [ $# -ne 6 ]; 
 then
-    echo "Usage: $0 <Username> <Password> <Install dir> <Port> <install_plpgsql>"
+    echo "Usage: $0 <OSUsername> <SuperUsername> <Password> <Install dir> <Port> <install_plpgsql>"
     exit 127
 fi
-
-USERNAME=$1
-PASSWORD=$2
-INSTALLDIR=$3
-PORT=$4
-INSTALL_PLPGSQL=$5
+OSUSERNAME=$1
+SUPERUSERNAME=$2
+PASSWORD=$3
+INSTALLDIR=$4
+PORT=$5
+INSTALL_PLPGSQL=$6
 
 # Exit code
 WARN=0
@@ -37,19 +37,19 @@ _warn() {
 # Create a password file for later
 touch /tmp/pgpass.$$ || _die "Failed to create the password file (/tmp/pgpass.$$)"
 chmod 0600 /tmp/pgpass.$$ || _die "Failed to set the permissions on the password file (/tmp/pgpass.$$)"
-echo "localhost:$PORT:*:$USERNAME:$PASSWORD" > /tmp/pgpass.$$ || _die "Failed to write the password file (/tmp/pgpass.$$)"
-chown $USERNAME:daemon /tmp/pgpass.$$ || _die "Failed to set the ownership of the password file (/tmp/pgpass.$$)"
+echo "localhost:$PORT:*:$SUPERUSERNAME:$PASSWORD" > /tmp/pgpass.$$ || _die "Failed to write the password file (/tmp/pgpass.$$)"
+chown $OSUSERNAME:daemon /tmp/pgpass.$$ || _die "Failed to set the ownership of the password file (/tmp/pgpass.$$)"
 
 if [ $INSTALL_PLPGSQL = "1" ];
 then 
     # Create the plpgsql language
     echo "Installing pl/pgsql in the template1 database..."
-    su - $USERNAME -c "PGPASSFILE=/tmp/pgpass.$$ $INSTALLDIR/bin/psql -U $USERNAME -p $PORT -c 'CREATE LANGUAGE plpgsql;' template1" || _warn "Failed to install pl/pgsql in the 'template1' database"
+    su - $OSUSERNAME -c "PGPASSFILE=/tmp/pgpass.$$ $INSTALLDIR/bin/psql -U $SUPERUSERNAME -p $PORT -c 'CREATE LANGUAGE plpgsql;' template1" || _warn "Failed to install pl/pgsql in the 'template1' database"
 fi
 
 # Install adminpack in the postgres database
 echo "Installing the adminpack module in the postgres database..."
-su - $USERNAME -c "PGPASSFILE=/tmp/pgpass.$$ $INSTALLDIR/bin/psql -U $USERNAME -p $PORT postgres < $INSTALLDIR/share/postgresql/contrib/adminpack.sql" || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
+su - $OSUSERNAME -c "PGPASSFILE=/tmp/pgpass.$$ $INSTALLDIR/bin/psql -U $SUPERUSERNAME -p $PORT postgres < $INSTALLDIR/share/postgresql/contrib/adminpack.sql" || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
 
 # Cleanup
 if [ -f /tmp/pgpass.$$ ];
