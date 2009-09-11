0ae736faa6f7661b16e2502530d88d8180c17bef
Ashesh Vashi <ashesh@ashesh-edb.edb.in>
2009-09-11 20:18:15 +0530

    * Showing information about missing unixODBC on linux/linux-x64 instead of throwing error * Run the lauchSBPUpdateMonitor.sh in background * Change directory (cd) in the launchSBPUpdateMonitor.sh to solve FB 12988

--- StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
+++ StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd INSTALL_DIR/UpdateManager.app/Contents/MacOS
 INSTALL_DIR/UpdateManager.app/Contents/MacOS/UpdateManager --server MONITOR_SERVER --execute "INSTALL_DIR/scripts/launchStackBuilderPlus.sh" &
 
