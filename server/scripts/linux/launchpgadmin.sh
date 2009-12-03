4bce31cdc4bea731923224f9cdae9af0a85ba110
Ashesh Vashi <asheshvashi@asheshvashi.edb.in>
2009-12-03 22:45:09 +0530

    Solved a bug, by modifying launchpgadmin.sh script, regarding not able to launch the pgadmin on linux/linux-x64, because PostgreSQL_Installation_dir/lib is no more in the ldconfig, and some of the library dependencies are missing.

--- server/scripts/linux/launchpgadmin.sh
+++ server/scripts/linux/launchpgadmin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=PG_INSTALLDIR/pgAdmin3/lib:$LD_LIBRARY_PATH G_SLICE=always-malloc PG_INSTALLDIR/pgAdmin3/bin/pgadmin3
+LD_LIBRARY_PATH=PG_INSTALLDIR/pgAdmin3/lib:PG_INSTALLDIR/lib:$LD_LIBRARY_PATH G_SLICE=always-malloc PG_INSTALLDIR/pgAdmin3/bin/pgadmin3
 
 
