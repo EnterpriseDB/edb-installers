58b8ff88773edc19b02248fd6ba1b5073d026243
Dharamendra Goyal <dharamendra.goyal@enterprisedb.com>
2010-03-10 19:40:10 +0530

    Windows PPHQ Services are started and stopped now via windows service manager. Fixed Case 14679:   [PPHQ Server]Stop Service o/p (via menu pick) NOT Stopping the Server

--- pphq/build-windows.sh
+++ pphq/build-windows.sh
@@ -99,7 +99,8 @@ _postprocess_pphq_windows() {
     # Copy in the menu pick images
     mkdir -p $PPHQ_STAGING/scripts/images || _die "Failed to create a directory for the menu pick images"
     cp $PPHQ_DIR/scripts/windows/runProgram.vbs $PPHQ_STAGING/scripts/ || _die "Failed to copy runProgram.vbs script"
-    cp $PPHQ_DIR/scripts/windows/shortPathName.vbs $PPHQ_STAGING/scripts/ || _die "Failed to copy runProgram.vbs script"
+    cp $PPHQ_DIR/scripts/windows/serverctl.vbs $PPHQ_STAGING/scripts/ || _die "Failed to copy serverctl.vbs script"
+    cp $PPHQ_DIR/scripts/windows/shortPathName.vbs $PPHQ_STAGING/scripts/ || _die "Failed to copy shortPathName.vbs script"
     cp $PPHQ_DIR/resources/*.ico $PPHQ_STAGING/scripts/images || _die "Failed to copy the menu pick images (resources/*.png)"
 
     # Build the installer
