7d0bdb784aba610e575f22a97d90ca374e947ceb
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2010-03-19 21:50:04 +0530

    Added code to copy the libpq to the build directory (Hack for psqlODBC-08.04.0200)

--- psqlODBC/build-osx.sh
+++ psqlODBC/build-osx.sh
@@ -55,6 +55,9 @@ _build_psqlODBC_osx() {
     SOURCE_DIR=$PG_PATH_OSX/psqlODBC/source/psqlODBC.osx
     cd $SOURCE_DIR
 
+    #Hack for psqlODBC-08.04.0200 
+    cp $PG_PGHOME_OSX/lib/libpq.5.dylib . || _die "Failed to copy the libpq to the build directory"
+ 
     echo "Configuring psqlODBC sources for intel"
     CFLAGS="$PG_ARCH_OSX_CFLAGS -arch i386" LDFLAGS="-lssl" PATH="$PG_PGHOME_OSX/bin:$PATH" ./configure --disable-dependency-tracking --with-iodbc --prefix="$PG_STAGING" || _die "Could not configuring psqlODBC sources for intel"
 
