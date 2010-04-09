daa771f57278737f35bfc971c54971f944a5f0d1
Dave Page <dpage@pgadmin.org>
2010-04-09 10:59:48 +0100

    Rewrite the getlocales.sh script which could be insanely slow in some situations, and could hide perfectly valid locales.

--- server/scripts/osx/getlocales.sh
+++ server/scripts/osx/getlocales.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-# PostgreSQL server locales script for OSX (returns valid locales on the system)
+# PostgreSQL server locales script (returns valid locales on the system)
 # Dave Page, EnterpriseDB
 
 #Check the command line
@@ -10,14 +10,17 @@ then
     exit 127
 fi
 
+# Use case-insensitive matching
+shopt -s nocasematch
+
 # OK, let's have the complete list then
 _encoding_is_valid()
 {
-    ENCODINGS="abc alt euccn eucjis2004 eucjp euckr euctw iso88591 iso885910 iso885913 iso885914 iso885915 iso885916 iso88592 iso88593 iso88594 iso88595 iso88596 iso88597 iso88598 iso88599 koi8 koi8r latin1 latin10 latin2 latin3 latin4 latin5 latin6 latin7 latin8 latin9 muleinternal sqlascii tcvn tcvn5712 unicode utf8 vscii"
+    ENCODINGS="utf8 iso88591 iso885910 iso885913 iso885914 iso885915 iso885916 iso88592 iso88593 iso88594 iso88595 iso88596 iso88597 iso88598 iso88599 abc alt euccn eucjis2004 eucjp euckr euctw koi8 koi8r latin1 latin10 latin2 latin3 latin4 latin5 latin6 latin7 latin8 latin9 muleinternal sqlascii tcvn tcvn5712 unicode vscii"
 
     for ENCODING in $ENCODINGS
     do
-        if [ "x$ENCODING" = "x$1" ];
+        if [[ $ENCODING =~ ^$1$ ]]
         then
             return 1
         fi
@@ -27,52 +30,23 @@ _encoding_is_valid()
 }
 
 # Echo the locales
-for LOCALE in `locale -a | LC_CTYPE=C grep -v -e "[^a-zA-Z0-9@\.\-\_]"`
+for LOCALE in `locale -a | grep "\."`
 do
-    if [ x`echo $LOCALE | grep @` = "x$LOCALE" ];
+    if [[ $LOCALE =~ ([^\.]+)\.([^@]+) ]]
 	then
-        if [ x`echo $LOCALE | grep "\."` = "x$LOCALE" ];
-    	then
-		    # xxx.yyy@zzz locales
-		    LCNAME=`echo $LOCALE | awk 'BEGIN {FS="[.@]"};  {print \$1}'`
-			ENCNAME=`echo $LOCALE | awk 'BEGIN {FS="[.@]"};  {print \$2}'`
-			MODNAME=`echo $LOCALE | awk 'BEGIN {FS="[.@]"};  {print \$3}'`
-		else
-		    # xxx@zzz locales
-		    LCNAME=`echo $LOCALE | awk 'BEGIN {FS="[@]"};  {print \$1}'`
-			ENCNAME=""
-			MODNAME=`echo $LOCALE | awk 'BEGIN {FS="[@]"};  {print \$3}'`
-		fi
-	else
-	    # xxx.yyy locales
-        LCNAME=`echo $LOCALE | awk 'BEGIN {FS="."};  {print \$1}'`
-        ENCNAME=`echo $LOCALE | awk 'BEGIN {FS="."};  {print \$2}'`
-		MODNAME=""
-	fi
-	
-	ENCABBR=`echo $ENCNAME | sed -e 's/-//g' | tr "[A-Z]" "[a-z]"`
-	ENCCLEAN=`echo $ENCNAME | sed -e 's/-/xxDASHxx/g'`
-	LCCLEAN=`echo $LCNAME | sed -e 's/_/xxUSxx/g'`
+	    ENCODING=${BASH_REMATCH[2]}
+		ENCODING=${ENCODING//-/}
 
-	if [ "x$ENCNAME" = "x" ];
-	then
-	    if [ "x$MODNAME" = "x" ];
-		then
-		    echo "$LCCLEAN=$LCNAME"
-		else
-		    echo "${LCCLEAN}xxATxx${MODNAME}=$LCNAME@$MODNAME"
-		fi
-	else
-		_encoding_is_valid $ENCABBR
+	    _encoding_is_valid $ENCODING
+		
 		if [ $? -eq 1 ];
 		then
-			if [ "x$MODNAME" = "x" ];
-		    then
-			    echo "${LCCLEAN}xxDOTxx${ENCCLEAN}=$LCNAME.$ENCNAME"
-			else
-			    echo "${LCCLEAN}xxDOTxx${ENCCLEAN}xxATxx${MODNAME}=$LCNAME.$ENCNAME@$MODNAME"
-			fi
+		    ENCODEDNAME=${LOCALE//-/xxDASHxx}
+			ENCODEDNAME=${ENCODEDNAME//_/xxUSxx}
+			ENCODEDNAME=${ENCODEDNAME//@/xxATxx}
+			ENCODEDNAME=${ENCODEDNAME//./xxDOTxx}
+	        echo "$ENCODEDNAME=$LOCALE"
 		fi
-	fi
+    fi
 done
 
