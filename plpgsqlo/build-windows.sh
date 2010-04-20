6683f9e53df4007d3a3059d42aa093a7f018b43d
Dharamendra Goyal <dharamendra.goyal@enterprisedb.com>
2010-04-20 15:19:12 +0530

    Adding plpgsqlo.sql and some code refactoring to 8.4 branch

--- plpgsqlo/build-windows.sh
+++ plpgsqlo/build-windows.sh
@@ -16,15 +16,15 @@ _prep_plpgsqlo_windows() {
     fi
 
     # create a copy of the plpgsql tree
-    cp -R postgres.windows/src/pl/plpgsql postgres.windows/src/pl/plpgsqlo || _die "Failed to create copy of plpgsql tree (postgres.windows/src/pl/plpgsql)"
+    cp -R postgresql-$PG_TARBALL_POSTGRESQL/src/pl/plpgsql postgres.windows/src/pl/plpgsqlo || _die "Failed to create copy of plpgsql tree (postgresql-$PG_TARBALL_POSTGRESQL/src/pl/plpgsql)"
     grep -irl plpgsql postgres.windows/src/pl/plpgsqlo |xargs sed -i .bak 's/\([pP][lL][pP][gG][sS][qQ][lL]\)/\1o/g'
     grep -rl PLPGSQLo_ postgres.windows/src/pl/plpgsqlo |xargs sed -i .bak 's/\(PLPGSQL\)o/\1/g'
     mv postgres.windows/src/pl/plpgsqlo/src/plpgsql.h postgres.windows/src/pl/plpgsqlo/src/plpgsqlo.h || _die "Failed to move plpgsql.h to plpgsqlo.h"
     # Copy files from pg-sources into plpgsqlo
-    cp $WD/server/source/postgres.windows/src/backend/utils/adt/encode.c $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy encode.c"
-    cp $WD/server/source/postgres.windows/contrib/pgcrypto/md5.c $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy md5.c"
-    cp $WD/server/source/postgres.windows/contrib/pgcrypto/md5.h $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy md5.h"
-    cp $WD/server/source/postgres.windows/contrib/pgcrypto/px.h $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy px.h"
+    cp $WD/server/source/postgresql-$PG_TARBALL_POSTGRESQL/src/backend/utils/adt/encode.c $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy encode.c"
+    cp $WD/server/source/postgresql-$PG_TARBALL_POSTGRESQL/contrib/pgcrypto/md5.c $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy md5.c"
+    cp $WD/server/source/postgresql-$PG_TARBALL_POSTGRESQL/contrib/pgcrypto/md5.h $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy md5.h"
+    cp $WD/server/source/postgresql-$PG_TARBALL_POSTGRESQL/contrib/pgcrypto/px.h $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy px.h"
     # copy wrap.c and wrap.h in plpgsqlo. These 2 files are taken from edb sources.
     cp $WD/plpgsqlo/resources/wrap.c $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy wrap.c file for plpgsqlo obfuscation"
     cp $WD/plpgsqlo/resources/wrap.h $WD/server/source/postgres.windows/src/pl/plpgsqlo/src || _die "Failed to copy wrap.h file for plpgsqlo obfuscation"
@@ -46,6 +46,12 @@ _prep_plpgsqlo_windows() {
       rm -rf $WD/plpgsqlo/staging/windows || _die "Couldn't remove the existing staging directory"
     fi
     mkdir $WD/plpgsqlo/staging/windows || _die "Couldn't create staging directory for windows"
+
+    echo "Creating staging share directory ($WD/plpgsqlo/staging/windows/share)"
+    mkdir -p $WD/plpgsqlo/staging/windows/share || _die "Couldn't create the staging share directory"
+    chmod ugo+w $WD/plpgsqlo/staging/windows/share || _die "Couldn't set the permissions on the staging share directory"
+    echo "Copying plpgsqlo.sql to staging share directory"
+    cp $WD/plpgsqlo/resources/plpgsqlo.sql $WD/plpgsqlo/staging/windows/share || _die "Couldn't copy plpgsqlo.sql to staging share directory"
 
     ssh $PG_SSH_WINDOWS "cd $PG_PATH_WINDOWS; cmd /c if EXIST plpgsqlo.zip del /S /Q plpgsqlo.zip" || _die "Couldn't remove the $PG_PATH_WINDOWS\\plpgsqlo.zip on Windows VM"
     ssh $PG_SSH_WINDOWS "cd $PG_PATH_WINDOWS; cmd /c if EXIST plpgsqlo.staging rd /S /Q plpgsqlo.staging" || _die "Couldn't remove the $PG_PATH_WINDOWS\\plpgsqlo.staging directory on Windows VM"
