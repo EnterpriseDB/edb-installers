d29c863c614df26d353ddd3fb2310ef97abd6f01
Buildfarm <buildfarm@jaypee.ox.uk.enterprisedb.com>
2010-08-04 12:11:51 +0100

    Fixed Case 16437:Menu Pick for Postgis Docs does not open the correct page

--- PostGIS/scripts/linux/launchPostGISDocs.sh
+++ PostGIS/scripts/linux/launchPostGISDocs.sh
@@ -3,6 +3,7 @@
 INSTALLDIR="INSTALL_DIR"
 DOCDIR="DOC_DIR"
 URL="file://$DOCDIR/postgis/postgis.html"
+chmod 755 $DOCDIR/postgis/postgis.html
 
 "$INSTALLDIR/PostGIS/scripts/launchbrowser.sh" $URL
 
