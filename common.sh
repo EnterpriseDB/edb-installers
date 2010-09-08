e218f46a40cad2fdcd4b4d51c7a6d00cff5c172b
Dave Page <dpage@pgadmin.org>
2010-09-08 09:49:36 +0100

    Ensure the Windows code signing tool can select the most appropriate certificate from the store, and doesn't barf if more than one is found.

--- common.sh
+++ common.sh
@@ -168,7 +168,7 @@ win32_sign()
                return
             fi
             NOT_SIGNED=0
-            ssh $PG_SSH_WINDOWS "cmd /c \"$PG_SIGNTOOL_WINDOWS\" sign /t http://tsa.starfieldtech.com $PG_PATH_WINDOWS/$FILENAME" || NOT_SIGNED=1
+            ssh $PG_SSH_WINDOWS "cmd /c \"$PG_SIGNTOOL_WINDOWS\" sign /a /t http://tsa.starfieldtech.com $PG_PATH_WINDOWS/$FILENAME" || NOT_SIGNED=1
             COUNT=`expr $COUNT + 1`
         done
         scp $PG_SSH_WINDOWS:$PG_PATH_WINDOWS/$FILENAME $WD/output/$FILENAME || _die "Failed to copy the installer from the windows host after signing ($FILENAME)"
