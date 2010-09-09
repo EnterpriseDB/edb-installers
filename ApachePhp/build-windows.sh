0a4420ccf01de8367ddc6db10cc7ffba921ce687
Dharamendra Goyal <dharamendra.goyal@enterprisedb.com>
2010-09-09 14:37:26 +0530

    correcting patch command as per the patch

--- ApachePhp/build-windows.sh
+++ ApachePhp/build-windows.sh
@@ -34,7 +34,8 @@ _prep_ApachePhp_windows() {
     cp -R php-$PG_VERSION_PHP php.windows || _die "Couldn't copy sources for php (php-$PG_VERSION_PHP to php.windows)"
     if [ x"$PG_VERSION_PHP" = x"5.3.3" -a -f "$WD/tarballs/php-$PG_VERSION_PHP-win32.patch" ]; then
         cp php.windows/win32/readdir.c php.windows/ext/mcrypt/readdirc
-        patch -p0 < $WD/tarballs/php-$PG_VERSION_PHP-win32.patch
+        cd php.windows
+        patch -p1 < $WD/tarballs/php-$PG_VERSION_PHP-win32.patch
     fi
 
     cd $WD/ApachePhp/source
