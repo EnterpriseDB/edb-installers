e5fc5274bf07ae200944ce2f51e8f37cccc8f931
Usman Saleem <usman.saleem@enterprisedb.com>
2010-10-05 16:20:15 +0500

    Fixing launchStackBuilderPlus.sh for Solaris. FB 16744. Launch runStackBuilderPlus.sh without xterm if xterm is not avaiable on path. This happens when user do 'su'.

--- StackBuilderPlus/scripts/solaris/launchStackBuilderPlus.sh
+++ StackBuilderPlus/scripts/solaris/launchStackBuilderPlus.sh
@@ -3,6 +3,15 @@
 # PostgreSQL stackbuilderplus launch script for Linux
 # Ashesh Vashi, EnterpriseDB
 
-xterm -e "INSTALL_DIR/scripts/runStackBuilderPlus.sh" $*
+XTERM = `which xterm`
 
-
+if [ -e "$XTERM" ];
+then
+    $XTERM -e "INSTALL_DIR/scripts/runStackBuilderPlus.sh" $*
+else
+    # xterm not available, so we run the runStackBuilderPlus.sh directly.
+    # As in solaris, we are not concerned about menu picks and thus this script can only be launched via terminal.
+    # Also in case of root/super user launching this script, this will correctly open the StackbuilderPlus.
+    # Note: In Solaris, xterm is in path of a normal user, but when he does a 'su', The PATH env changes and thus, xterm wont be in PATH
+    "INSTALL_DIR/scripts/runStackBuilderPlus.sh" $*
+fi
