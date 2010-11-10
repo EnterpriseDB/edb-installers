337cb621e7f49f152a9a5a6660d042d5ce826300
Yaser Raja <yaser.raja@enterprisedb.com>
2010-11-10 20:13:58 +0500

    FB17122: Stop passing switch --server to UM. This option is no more supported.

--- StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
+++ StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd INSTALL_DIR/UpdateManager.app/Contents/MacOS
-INSTALL_DIR/UpdateManager.app/Contents/MacOS/UpdateManager --server MONITOR_SERVER --execute "/Applications/BRANDING/StackBuilder Plus.app/Contents/MacOS/applet" &
+INSTALL_DIR/UpdateManager.app/Contents/MacOS/UpdateManager --execute "/Applications/BRANDING/StackBuilder Plus.app/Contents/MacOS/applet" &
 
