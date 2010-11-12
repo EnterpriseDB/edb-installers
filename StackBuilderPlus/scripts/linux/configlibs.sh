e39a0bebd2e02ae26701457dd800fc52331cbe91
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2010-11-12 11:51:12 +0530

    Fixed the configlibs.sh, so that it removes the libraries present in the system

--- StackBuilderPlus/scripts/linux/configlibs.sh
+++ StackBuilderPlus/scripts/linux/configlibs.sh
@@ -7,49 +7,32 @@ _process_libs() {
 
   lib_dir=$1
   libname=$2
+  platform=@@PLATFORM@@   
   
   # Remove the libraries that are already present in the system.
   cd $lib_dir
   library_list=`find . -name "$libname*"`
   standard_lib64_path="/lib64 /usr/lib64 /usr/local/lib64"
-  standard_lib_path="/lib /usr/lib /usr/local/lib"
+  standard_lib32_path="/lib /usr/lib /usr/local/lib"
 
+  if [ $platform = "linux64" ]; then
+    standard_lib_path=$standard_lib64_path
+  else
+    standard_lib_path=$standard_lib32_path
+  fi
+  
   for library in $library_list
   do
-    found_at_dir=
-    flag=
-    library=`basename $library`  
-    if [ -d "/lib64" ]; then 
-      for stdlibdir in $standard_lib64_path
-      do
-        if [ x"$flag" = x"" ]
-        then
-          flag=`ls $stdlibdir/$library`
-          found_at_dir=$flag
-        else
-          break
-        fi
-      done
-    else
-      for stdlibdir in $standard_lib_path
-      do
-        if [ "x$flag" = "x" ]
-        then
-          flag=`ls $stdlibdir/$library`
-          found_at_dir=$stdlibdir
-        else
-          break
-        fi
-      done
-    fi
-
-    # If found delete the library from the INSTALLDIR/lib 
-    if [ x"$flag" != x"" ]
-    then
-      echo "NOTE: Found '$library' in '$found_at_dir' on the system"
-      echo "      Removing it from the '$lib_dir'."
-      rm -f $library || _die "Failed to remove the library $library"
-    fi
+    lib=`basename $library`  
+    for stdlibdir in $standard_lib_path
+    do
+      if [ -e $stdlibdir/$lib ]; then
+        echo "NOTE: Found '$lib' in '$stdlibdir' on the system"
+        echo "      Removing it from the '$lib_dir'."
+        rm -f $lib_dir/$lib || _die "Failed to remove the library $library"
+        break
+      fi
+    done
   done
 
 }
