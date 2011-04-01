677aa68540a3f0a6092b7f6828e49c59e9230430
Dave Page <dpage@pgadmin.org>
2011-04-01 10:15:31 +0100

    Add 9.1 to the autobuild.

--- autobuild.sh
+++ autobuild.sh
@@ -54,6 +54,29 @@ ssh buildfarm@builds.enterprisedb.com mkdir -p /var/www/html/builds/pgInstaller/
 echo "Uploading output to /var/www/html/builds/pgInstaller/$DATE/9.0 on the builds server" >> autobuild.log
 scp output/* buildfarm@builds.enterprisedb.com:/var/www/html/builds/pgInstaller/$DATE/9.0 >> autobuild.log 2>&1
 
+# Clear out 9.0 output
+echo "Cleaning up 9.0 output" >> autobuild.log
+rm -rf output/* >> autobuild.log 2>&1
+
+# Switch to REL-9_1 branch
+echo "Switching to REL-9_1 branch" >> autobuild.log
+/opt/local/bin/git checkout REL-9_1 >> autobuild.log 2>&1
+
+# Self update
+echo "Updating REL-9_1 branch build system" >> autobuild.log
+/opt/local/bin/git pull >> autobuild.log 2>&1
+
+# Run the build, and dump the output to a log file
+echo "Running the build (REL-9_1) " >> autobuild.log
+./build.sh > output/build-91.log 2>&1
+
+# Create a remote directory and upload the output.
+echo "Creating /var/www/html/builds/pgInstaller/$DATE/9.1 on the builds server" >> autobuild.log
+ssh buildfarm@builds.enterprisedb.com mkdir -p /var/www/html/builds/pgInstaller/$DATE/9.1 >> autobuild.log 2>&1
+
+echo "Uploading output to /var/www/html/builds/pgInstaller/$DATE/9.1 on the builds server" >> autobuild.log
+scp output/* buildfarm@builds.enterprisedb.com:/var/www/html/builds/pgInstaller/$DATE/9.1 >> autobuild.log 2>&1
+
 echo "#######################################################################" >> autobuild.log
 echo "Build run completed at `date`" >> autobuild.log
 echo "#######################################################################" >> autobuild.log
