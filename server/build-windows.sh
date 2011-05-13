14dd4cafcf009c27d45ebe4f7ed0cb39879eaf24
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2011-05-13 18:41:00 +0530

    sign the stackbuilder executable (corrected the path)

--- server/build-windows.sh
+++ server/build-windows.sh
@@ -401,7 +401,7 @@ EOT
     rm $WD/server/staging/windows/output.zip
 
 	# sign stackbuilder
-	win32_sign "stackbuilder.exe" "$WD/server/staging/windows/StackBuilder/bin"
+	win32_sign "stackbuilder.exe" "$WD/server/staging/windows/bin"
 	
 	# Install the PostgreSQL docs
 	mkdir -p $WD/server/staging/windows/doc/postgresql/html || _die "Failed to create the doc directory"
