6699fdfcae2322d7dbab4b054464e51d110e5981
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2011-07-12 19:49:15 +0530

    Porting sql-protect to windows-x64

--- sqlprotect/build-windows.sh
+++ sqlprotect/build-windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-    
+
 ################################################################################
 # Build preparation
 ################################################################################
@@ -8,18 +8,18 @@
 _prep_sqlprotect_windows() {
 
     cd $WD/server/source
-	
+
     # Remove any existing sqlprotect directory that might exist, in server
     if [ -e postgres.windows/contrib/SQLPROTECT ];
     then
       echo "Removing existing sqlprotect directory"
       rm -rf postgres.windows/contrib/SQLPROTECT || _die "Couldn't remove the existing sqlprotect directory"
     fi
-	
+
     # create a copy of the sqlprotect tree
 	cd postgres.windows/contrib
     git clone ssh://pginstaller@cvs.enterprisedb.com/git/SQLPROTECT
-	
+
     # Remove any existing staging directory that might exist, and create a clean one
     if [ -e $WD/sqlprotect/staging/windows ];
     then
@@ -30,7 +30,7 @@ _prep_sqlprotect_windows() {
     echo "Creating staging directory ($WD/sqlprotect/staging/windows)"
     mkdir -p $WD/sqlprotect/staging/windows/sqlprotect || _die "Couldn't create the staging directory"
     chmod ugo+w $WD/sqlprotect/staging/windows || _die "Couldn't set the permissions on the staging directory"
-	
+
     echo "Archiving sqlprotect sources"
 	cd $WD/server/source
     if [ -e sqlprotect.zip ];
@@ -53,7 +53,7 @@ _prep_sqlprotect_windows() {
     scp sqlprotect.zip $PG_SSH_WINDOWS:$PG_PATH_WINDOWS || _die "Couldn't copy the sqlprotect archive to windows VM (sqlprotect.zip)"
     ssh $PG_SSH_WINDOWS "cd $PG_PATH_WINDOWS; cmd /c  unzip sqlprotect.zip" || _die "Couldn't extract postgresql archieve on windows VM (sqlprotect.zip)"
     chmod -R ugo+r $WD/sqlprotect/staging/windows
- 
+
 }
 
 ################################################################################
@@ -68,17 +68,24 @@ _build_sqlprotect_windows() {
    ssh $PG_SSH_WINDOWS "mkdir -p $PG_PATH_WINDOWS/sqlprotect.staging/lib/postgresql" || _die "Failed to create the lib directory"
    ssh $PG_SSH_WINDOWS "mkdir -p $PG_PATH_WINDOWS/sqlprotect.staging/share" || _die "Failed to create the share directory"
    ssh $PG_SSH_WINDOWS "mkdir -p $PG_PATH_WINDOWS/sqlprotect.staging/doc" || _die "Failed to create the doc directory"
-   
+
    ssh $PG_SSH_WINDOWS "cp $PG_PATH_WINDOWS/postgres.windows/release/sqlprotect/sqlprotect.dll $PG_PATH_WINDOWS/sqlprotect.staging/lib/postgresql" || _die "Failed to copy sqlprotect.dll to staging directory"
    ssh $PG_SSH_WINDOWS "cp $PG_PATH_WINDOWS/postgres.windows/contrib/SQLPROTECT/sqlprotect.sql $PG_PATH_WINDOWS/sqlprotect.staging/share" || _die "Failed to copy sqlprotect.sql to staging directory"
    ssh $PG_SSH_WINDOWS "cp $PG_PATH_WINDOWS/postgres.windows/contrib/SQLPROTECT/README-sqlprotect.txt $PG_PATH_WINDOWS/sqlprotect.staging/doc" || _die "Failed to copy README-sqlprotect.txt to staging directory"
-   
+
    # Zip up the installed code, copy it back here, and unpack.
    echo "Copying sqlprotect build tree to Unix host"
    ssh $PG_SSH_WINDOWS "cd $PG_PATH_WINDOWS\\\\sqlprotect.staging; cmd /c zip -r ..\\\\sqlprotect-staging.zip *" || _die "Failed to pack the built source tree ($PG_SSH_WINDOWS:$PG_PATH_WINDOWS/sqlprotect.staging)"
    scp $PG_SSH_WINDOWS:$PG_PATH_WINDOWS/sqlprotect-staging.zip $WD/sqlprotect/staging/windows || _die "Failed to copy the built source tree ($PG_SSH_WINDOWS:$PG_PATH_WINDOWS/sqlprotect-staging.zip)"
    unzip $WD/sqlprotect/staging/windows/sqlprotect-staging.zip -d $WD/sqlprotect/staging/windows || _die "Failed to unpack the built source tree ($WD/staging/windows/sqlprotect-staging.zip)"
    rm $WD/sqlprotect/staging/windows/sqlprotect-staging.zip
+
+    # Remove any existing sqlprotect directory that might exist, in server
+    if [ -e postgres.windows/contrib/SQLPROTECT ];
+    then
+      echo "Removing existing sqlprotect directory"
+      rm -rf postgres.windows/contrib/SQLPROTECT || _die "Couldn't remove the existing sqlprotect directory"
+    fi
 
 }
 
@@ -99,6 +106,8 @@ _postprocess_sqlprotect_windows() {
     fi
     cp installer.xml installer-win.xml
 
+    _replace @@WIN64MODE@@ "0" installer-win.xml || _die "Failed to replace the WIN64MODE setting in the installer.xml"
+    _replace @@WINDIR@@ "windows" installer-win.xml || _die "Failed to replace the WINDIR setting in the installer.xml"
     _replace "registration_plus_component" "registration_plus_component_processed" installer-win.xml || _die "Failed to replace the registration_plus component file name"
 
     # Build the installer
@@ -106,7 +115,7 @@ _postprocess_sqlprotect_windows() {
 
     # Sign the installer
     win32_sign "sqlprotect-$PG_VERSION_SQLPROTECT-$PG_BUILDNUM_SQLPROTECT-windows.exe"
-	
+
     cd $WD
 
 }
