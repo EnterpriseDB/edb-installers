e7b438f284d2b7350cbeb32598b831d7bdad2bab
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2011-08-30 17:43:24 +0530

    Removing the package Drupal7

--- Drupal7/scripts/linux/launchDrupal7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=`grep APACHE_PORT /etc/postgres-reg.ini | cut -f 2 -d "="`
-INSTALLDIR="INSTALL_DIR"
-
-URL=http://localhost:$PORT/Drupal7
-
-"$INSTALLDIR/Drupal7/scripts/launchbrowser.sh" $URL
-
