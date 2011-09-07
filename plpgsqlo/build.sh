f1d10c5c6b37deab1265089eca632690559032cf
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2011-09-07 16:42:16 +0530

    Ported plpgsqlo to the windows-x64

--- plpgsqlo/build.sh
+++ plpgsqlo/build.sh
@@ -3,7 +3,7 @@
 # Read the various build scripts
 
 # Mac OS X
-if [ $PG_ARCH_OSX = 1 ]; 
+if [ $PG_ARCH_OSX = 1 ];
 then
     source $WD/plpgsqlo/build-osx.sh
 fi
@@ -25,7 +25,13 @@ if [ $PG_ARCH_WINDOWS = 1 ];
 then
     source $WD/plpgsqlo/build-windows.sh
 fi
-    
+
+# Windows-x64
+if [ $PG_ARCH_WINDOWS_X64 = 1 ];
+then
+    source $WD/plpgsqlo/build-windows-x64.sh
+fi
+
 ################################################################################
 # Build preparation
 ################################################################################
@@ -34,9 +40,12 @@ _prep_plpgsqlo() {
 
     # Per-platform prep
     cd $WD
-    
+
+    # Clean up the registration plus xmls
+    rm -f $WD/plpgsqlo/staging/*.xml
+
     # Mac OS X
-    if [ $PG_ARCH_OSX = 1 ]; 
+    if [ $PG_ARCH_OSX = 1 ];
     then
         _prep_plpgsqlo_osx || exit 1
     fi
@@ -58,7 +67,13 @@ _prep_plpgsqlo() {
     then
         _prep_plpgsqlo_windows || exit 1
     fi
-    
+
+    # Windows-x64
+    if [ $PG_ARCH_WINDOWS_X64 = 1 ];
+    then
+        _prep_plpgsqlo_windows_x64 || exit 1
+    fi
+
 }
 
 ################################################################################
@@ -68,12 +83,12 @@ _prep_plpgsqlo() {
 _build_plpgsqlo() {
 
     # Mac OSX
-    if [ $PG_ARCH_OSX = 1 ]; 
+    if [ $PG_ARCH_OSX = 1 ];
     then
         _build_plpgsqlo_osx || exit 1
     fi
 
-    # Linux 
+    # Linux
     if [ $PG_ARCH_LINUX = 1 ];
     then
         _build_plpgsqlo_linux || exit 1
@@ -90,6 +105,13 @@ _build_plpgsqlo() {
     then
         _build_plpgsqlo_windows || exit 1
     fi
+
+    # Windows-x64
+    if [ $PG_ARCH_WINDOWS_X64 = 1 ];
+    then
+        _build_plpgsqlo_windows_x64 || exit 1
+    fi
+
 }
 
 ################################################################################
@@ -113,13 +135,13 @@ _postprocess_plpgsqlo() {
     _replace PG_VERSION_PLPGSQLO $PG_VERSION_PLPGSQLO installer.xml || _die "Failed to set the version in the installer project file (plpgsqlo/installer.xml)"
     _replace PG_BUILDNUM_PLPGSQLO $PG_BUILDNUM_PLPGSQLO installer.xml || _die "Failed to set the Build Number in the installer project file (plpgsqlo/installer.xml)"
     _replace PG_MAJOR_VERSION $PG_MAJOR_VERSION installer.xml || _die "Failed to set the version in the installer project file (plpgsqlo/installer.xml)"
-   
-    #_registration_postprocess(STAGING DIRECTORY, COMPONENT NAME, VERSION VARIABLE, INI, REGISTRY_PREFIX, REGISTRY_PREFIX_WIN, TEMP DIRECTORY, COMONENT TYPE) 
+
+    #_registration_postprocess(STAGING DIRECTORY, COMPONENT NAME, VERSION VARIABLE, INI, REGISTRY_PREFIX, REGISTRY_PREFIX_WIN, TEMP DIRECTORY, COMONENT TYPE)
     _registration_postprocess "$WD/plpgsqlo/staging"  "PLPGSQLO"         "plpgsqloVersion" "/etc/postgres-reg.ini" "plpgsqlo/$PG_MAJOR_VERSION" "plpgsqlo-$PG_MAJOR_VERSION" "plpgsqlo_installer" "plpgsqlo"
 
 
     # Mac OSX
-    if [ $PG_ARCH_OSX = 1 ]; 
+    if [ $PG_ARCH_OSX = 1 ];
     then
         _postprocess_plpgsqlo_osx || exit 1
     fi
@@ -135,10 +157,17 @@ _postprocess_plpgsqlo() {
     then
         _postprocess_plpgsqlo_linux_x64 || exit 1
     fi
-    
+
     # Windows
     if [ $PG_ARCH_WINDOWS = 1 ];
     then
         _postprocess_plpgsqlo_windows || exit 1
     fi
+
+    # Windows-x64
+    if [ $PG_ARCH_WINDOWS_X64 = 1 ];
+    then
+        _postprocess_plpgsqlo_windows_x64 || exit 1
+    fi
+
 }
