18df9418a7830449c172084ac94c97320f636955
Muhammad Haroon <muhammad.haroon@enterprisedb.com>
2011-09-21 22:12:58 +0500

    correcting a typo in last commit

--- StackBuilderPlus/scripts/hpux/runStackBuilderPlus.sh
+++ StackBuilderPlus/scripts/hpux/runStackBuilderPlus.sh
@@ -20,7 +20,7 @@ then
     chmod a+r /tmp/.parseUser1Xauth.txt
 
     echo "Please enter the root password when requested."
-    su - root -c "sh -c 'xauth add `cat /tmp/.parseUser1Xauth.txt`;export DISPLAY=`cat /tmp/.echoUser1DISPLAY.txt`;LD_LIBRARY_PATH=INSTALL_DIR/lib:$LD_LIBRARY_PATH; export GDK_PIXBUF_MODULE_FILE=../lib/gdk-pixbuf-2.0/2.10.0/loaders.cache;export GDK_PIXBUF_MODULEDIR=../lib/gdk-pixbuf-2.0/2.10.0/loaders/; cd INSTALL_DIR/bin; G_SLICE=always-malloc; INSTALL_DIR/bin/stackbuilderplus $*"
+    su - root -c "sh -c 'xauth add `cat /tmp/.parseUser1Xauth.txt`;export DISPLAY=`cat /tmp/.echoUser1DISPLAY.txt`;LD_LIBRARY_PATH=INSTALL_DIR/lib:$LD_LIBRARY_PATH; export GDK_PIXBUF_MODULE_FILE=../lib/gdk-pixbuf-2.0/2.10.0/loaders.cache;export GDK_PIXBUF_MODULEDIR=../lib/gdk-pixbuf-2.0/2.10.0/loaders/; cd INSTALL_DIR/bin; G_SLICE=always-malloc; INSTALL_DIR/bin/stackbuilderplus $*'"
     rm -f /tmp/.echoUser1DISPLAY.txt
     rm -f /tmp/.parseUser1Xauth.txt
 else
