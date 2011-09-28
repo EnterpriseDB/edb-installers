b57664a26b2891bc6a217b66f7586cbc0092a192
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2011-09-28 15:17:04 +0530

    Fixed the call to check the database existence script and fix the create templatedb script

--- PostGIS/scripts/windows/createtemplatedb.bat
+++ PostGIS/scripts/windows/createtemplatedb.bat
@@ -6,19 +6,17 @@
 @SET PGPASSWORD=%4
 @SET PGHOME=%5
 
-@SET PATH=%PGHOME%\bin;%PATH%
-
 REM Creating template postgis database
-createdb template_postgis
+"%PGHOME%\bin\createdb.exe" template_postgis
 
 REM Creating template postgis language
-createlang plpgsql template_postgis
+"%PGHOME%\bin\createlang.exe" plpgsql template_postgis
 
 REM Set the template flag in the pg_database table
-psql -c "UPDATE pg_database SET datistemplate='t' WHERE datname='template_postgis'"
+"%PGHOME%\bin\psql.exe" -c "UPDATE pg_database SET datistemplate='t' WHERE datname='template_postgis'"
 
 REM Creating template postgis functions
-psql -f "@@INSTALL_DIR@@\share\lwpostgis.sql" -d template_postgis
-psql -f "@@INSTALL_DIR@@\share\spatial_ref_sys.sql" -d template_postgis
+"%PGHOME%\bin\psql.exe" -f "@@INSTALL_DIR@@\share\lwpostgis.sql" -d template_postgis
+"%PGHOME%\bin\psql.exe" -f "@@INSTALL_DIR@@\share\spatial_ref_sys.sql" -d template_postgis
 
 
