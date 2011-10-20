7371fa742eed512ba8508354eea7e2cd7b01c074
Buildfarm <buildfarm@jaypee.ox.uk.enterprisedb.com>
2011-10-20 13:18:12 +0100

    Updated prepare.sh for AS91 HPUX.

--- binaries/prepare.sh
+++ binaries/prepare.sh
@@ -7,15 +7,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/opt/local/bin
 #CM folder name
 cm_dirname=autobuild
 echo $cmd_dirname
-ftplocation=ftp://mail.isb.pk.enterprisedb.com/Binaries/as90-binaries-for-installer/$cm_dirname
+ftplocation=ftp://mail.isb.pk.enterprisedb.com/Binaries/as91-binaries-for-installer/$cm_dirname
 echo $ftplocation
 
 #Move existing .tar.gz to a folder
 dirname=$(date +"%d%m%y-%H%M")
 mkdir ./$dirname
 if [ $PREP_HPUX = 1 ]; then
-  mv ./AS90-HPUX-shared.tar.gz ./$dirname/
-  wget "$ftplocation/AS90-HPUX-shared.tar.gz"
+  mv ./AS91-HPUX-shared.tar.gz ./$dirname/
+  wget "$ftplocation/AS91-HPUX-shared.tar.gz"
 
   # Make sure the download went okay.
   if [ $? -ne 0 ]
@@ -24,11 +24,11 @@ if [ $PREP_HPUX = 1 ]; then
       echo "Wget has errors fetching HPUX distro."
       exit 1 
   fi
-  rm -rf ./AS90-HPUX
-  if [ -f ./AS90-HPUX-shared.tar.gz ]; then
-    tar -xvzf ./AS90-HPUX-shared.tar.gz
+  rm -rf ./AS91-HPUX
+  if [ -f ./AS91-HPUX-shared.tar.gz ]; then
+    tar -xvzf ./AS91-HPUX-shared.tar.gz
   else
-    echo "File missing: AS90-HPUX-shared.tar.gz"
+    echo "File missing: AS91-HPUX-shared.tar.gz"
     exit 1
   fi
 fi
