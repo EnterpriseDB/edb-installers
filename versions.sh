a17e6a5165dba46f3da64cae1b30c287dbd90391
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2012-05-03 00:04:06 +0530

    Bumped the PG buildnum

--- versions.sh
+++ versions.sh
@@ -43,7 +43,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.1
-PG_MINOR_VERSION=3.1
+PG_MINOR_VERSION=3.2
 
 # Other package versions
 PG_VERSION_APACHE=2.2.22
