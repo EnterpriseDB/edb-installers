44c6b35fb5248d1e5d395fb4b778c6fb84e7a66c
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2012-06-01 06:57:37 +0530

    Bumped the PG version

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=8.3.18
+PG_TARBALL_POSTGRESQL=8.3.19
 PG_TARBALL_PGADMIN=1.8.4
 PG_TARBALL_DEBUGGER=0.93
 PG_TARBALL_PLJAVA=1.4.0
@@ -20,7 +20,7 @@ PG_BUILDNUM_SLONY=1
 #                     Minor version is revision.build. 
 
 PG_MAJOR_VERSION=8.3
-PG_MINOR_VERSION=18.2
+PG_MINOR_VERSION=19.1
 
 # Other package versions
 PG_VERSION_POSTGIS=1.3.6
