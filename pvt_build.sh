c4f25768789279bab8d917ba83006b3f92e16419
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2012-07-05 12:02:52 +0530

    Fix the pvt_build.sh for REL-9_2 branch

--- pvt_build.sh
+++ pvt_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -e $WD/pvt_settings.sh ]
+if [ -e $WD/pvt_settings.sh.REL-9_2 ]
 then
 
     # Source the private settings.    
-    source $WD/pvt_settings.sh.REL-9_1
+    source $WD/pvt_settings.sh.REL-9_2
 
     # Create the pvt_packages directory. If not exist.
     if [ ! -e $WD/pvt_packages ];
