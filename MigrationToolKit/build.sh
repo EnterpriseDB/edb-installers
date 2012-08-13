7c38f02f863c979a2d9b7ecc803b03b36de41677
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2012-08-13 16:51:07 +0530

    Enabled building Migration Toolkit for Solaris SPARC.

--- MigrationToolKit/build.sh
+++ MigrationToolKit/build.sh
@@ -32,11 +32,11 @@ then
     source $WD/MigrationToolKit/build-solaris-x64.sh
 fi
     
-## Solaris sparc
-#if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
-#then
-#    source $WD/MigrationToolKit/build-solaris-sparc.sh
-#fi
+# Solaris sparc
+if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
+then
+    source $WD/MigrationToolKit/build-solaris-sparc.sh
+fi
 
 ################################################################################
 # Build preparation
@@ -120,11 +120,11 @@ _prep_MigrationToolKit() {
         _prep_MigrationToolKit_solaris_x64 || exit 1
     fi
     
-#    # Solaris sparc
-#    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
-#    then
-#        _prep_MigrationToolKit_solaris_sparc || exit 1
-#    fi
+    # Solaris sparc
+    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
+    then
+        _prep_MigrationToolKit_solaris_sparc || exit 1
+    fi
 
 }
 
@@ -164,11 +164,11 @@ _build_MigrationToolKit() {
         _build_MigrationToolKit_solaris_x64 || exit 1
     fi
 
-#    # Solaris sparc
-#    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
-#    then
-#        _build_MigrationToolKit_solaris_sparc || exit 1
-#    fi
+    # Solaris sparc
+    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
+    then
+        _build_MigrationToolKit_solaris_sparc || exit 1
+    fi
 
 }
 
@@ -226,10 +226,10 @@ _postprocess_MigrationToolKit() {
         _postprocess_MigrationToolKit_solaris_x64 || exit 1
     fi
 
-#    # Solaris sparc
-#    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
-#    then
-#        _postprocess_MigrationToolKit_solaris_sparc || exit 1
-#    fi
+    # Solaris sparc
+    if [ $PG_ARCH_SOLARIS_SPARC = 1 ];
+    then
+        _postprocess_MigrationToolKit_solaris_sparc || exit 1
+    fi
     
 }
