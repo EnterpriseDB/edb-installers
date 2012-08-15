21f62c45aaff50b71920518e15d9bdb3d353a39c
Sachin Srivastava <sachin.srivastava@enterprisedb.com>
2012-08-15 09:35:57 +0530

    Bumped the PG version number

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=9.1.4
+PG_TARBALL_POSTGRESQL=9.1.5
 PG_TARBALL_PGADMIN=1.14.3
 PG_TARBALL_DEBUGGER=0.93
 PG_TARBALL_PLJAVA=1.4.0
@@ -43,7 +43,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.1
-PG_MINOR_VERSION=4.1
+PG_MINOR_VERSION=5.1
 
 # Other package versions
 PG_VERSION_APACHE=2.2.22
