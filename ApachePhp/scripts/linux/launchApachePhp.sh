3cfabaf021c8c9a2a131bb0443c29ce90b06a156
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2012-08-31 13:30:10 +0530

    Resolved FB#22037: ApachePHP launching issue on linux/linux-x64 & osx

--- ApachePhp/scripts/linux/launchApachePhp.sh
+++ ApachePhp/scripts/linux/launchApachePhp.sh
@@ -3,6 +3,10 @@
 
 PORT=`grep APACHE_PORT /etc/postgres-reg.ini | cut -f 2 -d "="`
 
+# Trim the Port value
+PORT="${PORT#"${PORT%%[![:space:]]*}"}"
+PORT="${PORT%"${PORT##[![:space:]]*}"}"
+
 URL=http://localhost:$PORT
 
 INSTALLDIR="INSTALL_DIR"
