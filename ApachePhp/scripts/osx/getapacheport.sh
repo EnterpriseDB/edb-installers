bd2b05703d1f3030aa1099eb57527a09ad492d04
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2012-08-31 13:25:37 +0530

    Resolved FB#22037: ApachePHP launching issue on linux/linux-x64 & osx

--- ApachePhp/scripts/osx/getapacheport.sh
+++ ApachePhp/scripts/osx/getapacheport.sh
@@ -2,5 +2,10 @@
 # Copyright (c) 2012, EnterpriseDB Corporation.  All rights reserved
 
 PORT=`grep APACHE_PORT /etc/postgres-reg.ini | cut -f 2 -d "="`
+
+# Trim the Port value
+PORT="${PORT#"${PORT%%[![:space:]]*}"}"
+PORT="${PORT%"${PORT##[![:space:]]*}"}"
+
 URL=http://localhost:$PORT
 echo $URL
