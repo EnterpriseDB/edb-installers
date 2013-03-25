9090395c504b935b4b6043e74ed169aaa73748f3
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2013-03-25 14:43:43 +0530

    Security fix refs #30203, #30205, #30223, #30208

--- server/scripts/osx/loadmodules.sh
+++ server/scripts/osx/loadmodules.sh
@@ -4,25 +4,25 @@
 # PostgreSQL server module load script for OSX
 
 #Check the command line
-if [ $# -ne 5 ]; 
+if [ $# -ne 4 ]; 
 then
-    echo "Usage: $0 <OSUsername> <SuperUsername> <Password> <Install dir> <Port>"
+    echo "Usage: $0 <OSUsername> <SuperUsername> <Install dir> <Port>"
     exit 127
 fi
 OSUSERNAME=$1
 SUPERUSERNAME=$2
-PASSWORD=$3
-INSTALLDIR=$4
-PORT=$5
+PASSWORD=$PGPASSWORD
+INSTALLDIR=$3
+PORT=$4
 
 # Exit code
 WARN=0
 
 # Error handlers
 _die() {
-    if [ -f /tmp/pgpass.$$ ];
+    if [ -f $INSTALLDIR/installer/server/pgpass.$$ ];
     then
-        rm -rf /tmp/pgpass.$$
+        rm -rf $INSTALLDIR/installer/server/pgpass.$$
     fi
     echo $1
     exit 1
@@ -34,19 +34,19 @@ _warn() {
 }
 
 # Create a password file for later
-touch /tmp/pgpass.$$ || _die "Failed to create the password file (/tmp/pgpass.$$)"
-chmod 0600 /tmp/pgpass.$$ || _die "Failed to set the permissions on the password file (/tmp/pgpass.$$)"
-echo "localhost:$PORT:*:$SUPERUSERNAME:$PASSWORD" > /tmp/pgpass.$$ || _die "Failed to write the password file (/tmp/pgpass.$$)"
-chown $OSUSERNAME:daemon /tmp/pgpass.$$ || _die "Failed to set the ownership of the password file (/tmp/pgpass.$$)"
+touch $INSTALLDIR/installer/server/pgpass.$$ || _die "Failed to create the password file ($INSTALLDIR/installer/server/pgpass.$$)"
+chmod 0600 $INSTALLDIR/installer/server/pgpass.$$ || _die "Failed to set the permissions on the password file ($INSTALLDIR/installer/server/pgpass.$$)"
+echo "localhost:$PORT:*:$SUPERUSERNAME:$PASSWORD" > $INSTALLDIR/installer/server/pgpass.$$ || _die "Failed to write the password file ($INSTALLDIR/installer/server/pgpass.$$)"
+chown $OSUSERNAME:daemon $INSTALLDIR/installer/server/pgpass.$$ || _die "Failed to set the ownership of the password file ($INSTALLDIR/installer/server/pgpass.$$)"
 
 # Install adminpack in the postgres database
 echo "Installing the adminpack module in the postgres database..."
-su - $OSUSERNAME -c "PGPASSFILE=/tmp/pgpass.$$ $INSTALLDIR/bin/psql -U $SUPERUSERNAME -p $PORT postgres < $INSTALLDIR/share/postgresql/contrib/adminpack.sql" || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
+su - $OSUSERNAME -c "PGPASSFILE=$INSTALLDIR/installer/server/pgpass.$$ $INSTALLDIR/bin/psql -U $SUPERUSERNAME -p $PORT postgres < $INSTALLDIR/share/postgresql/contrib/adminpack.sql" || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
 
 # Cleanup
-if [ -f /tmp/pgpass.$$ ];
+if [ -f $INSTALLDIR/installer/server/pgpass.$$ ];
 then
-    rm /tmp/pgpass.$$ || _warn "Failed to remove the password file (/tmp/pgpass.$$)"
+    rm $INSTALLDIR/installer/server/pgpass.$$ || _warn "Failed to remove the password file ($INSTALLDIR/installer/server/pgpass.$$)"
 fi
 
 echo "$0 ran to completion"
