c36ad915e9ae75e9b013496b5d54c74a4a17413a
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2013-06-11 18:18:36 +0530

    Fixing the updatemonitor issues fixes #30457, #30807, #30809

--- UpdateMonitor/scripts/osx/launchUpdateMonitor.sh
+++ UpdateMonitor/scripts/osx/launchUpdateMonitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2012, EnterpriseDB Corporation.  All rights reserved
 
-cd INSTALL_DIR/UpdateMonitor.app/Contents/MacOS
-INSTALL_DIR/UpdateMonitor.app/Contents/MacOS/UpdateManager --execute "/Applications/BRANDING/Application Stack Builder.app/Contents/MacOS/applet" &
+cd INSTALL_DIR/UpdateManager.app/Contents/MacOS
+INSTALL_DIR/UpdateManager.app/Contents/MacOS/UpdateManager --execute "/Applications/BRANDING/Application Stack Builder.app/Contents/MacOS/applet" &
 
