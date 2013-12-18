918d5e0009b59c5ee525f48fe0718f8c593e91ae
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2013-12-18 16:55:52 +0530

    fixes #32180. Remove the unsupported JDBC script from the Postgis

--- PostGIS/build-linux-x64.sh
+++ PostGIS/build-linux-x64.sh
@@ -220,8 +220,6 @@ _postprocess_PostGIS_linux_x64() {
     chmod ugo+x staging/linux-x64/scripts/launchbrowser.sh
     cp -R scripts/linux/launchPostGISDocs.sh staging/linux-x64/scripts/launchPostGISDocs.sh || _die "Failed to copy the launch scripts (scripts/linux-x64)"
     chmod ugo+x staging/linux-x64/scripts/launchPostGISDocs.sh
-    cp -R scripts/linux/launchJDBCDocs.sh staging/linux-x64/scripts/launchJDBCDocs.sh || _die "Failed to copy the launch scripts (scripts/linux-x64)"
-    chmod ugo+x staging/linux-x64/scripts/launchJDBCDocs.sh
 
     # Copy the XDG scripts
     mkdir -p staging/linux-x64/installer/xdg || _die "Failed to create a directory for the xdg scripts"
@@ -240,7 +238,6 @@ _postprocess_PostGIS_linux_x64() {
     cp resources/xdg/pg-postgresql.directory staging/linux-x64/scripts/xdg/pg-postgresql-$PG_VERSION_STR.directory || _die "Failed to copy a menu pick directory"
     cp resources/xdg/pg-postgis.directory staging/linux-x64/scripts/xdg/pg-postgis-$POSTGIS_VERSION_STR.directory || _die "Failed to copy a menu pick directory"
     cp resources/xdg/pg-launchPostGISDocs.desktop staging/linux-x64/scripts/xdg/pg-launchPostGISDocs-$POSTGIS_VERSION_STR.desktop || _die "Failed to copy a menu pick desktop"
-    cp resources/xdg/pg-launchPostGISJDBCDocs.desktop staging/linux-x64/scripts/xdg/pg-launchPostGISJDBCDocs-$POSTGIS_VERSION_STR.desktop || _die "Failed to copy a menu pick desktop"
 
     # Build the installer
     "$PG_INSTALLBUILDER_BIN" build installer.xml linux-x64 || _die "Failed to build the installer"
