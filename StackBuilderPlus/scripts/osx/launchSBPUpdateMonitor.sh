66be16a3328fc7c34939b93d419784b37e09dd34
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2014-01-03 15:28:52 +0530

    Updated copyright year.

--- StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
+++ StackBuilderPlus/scripts/osx/launchSBPUpdateMonitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012-2013, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2014, EnterpriseDB Corporation.  All rights reserved
 
 cd INSTALL_DIR/UpdateManager.app/Contents/MacOS
 INSTALL_DIR/UpdateManager.app/Contents/MacOS/UpdateManager --execute "/Applications/BRANDING/StackBuilder Plus.app/Contents/MacOS/applet" &
---
+++ copyright_date_change.sh
@@ -0,0 +1,15 @@
+
+BASENAME=`basename $0`
+YEAR=`date | awk '{print $NF}'`
+OLD_YEAR=`expr $YEAR - 1`
+WD=$PWD
+
+    for file in `find $WD -name "*.sh" -o -name "*.bat" -o -name "*.vbs" | grep -v $BASENAME | grep -v binaries | grep -v staging | grep -v source | xargs grep "EnterpriseDB Corp"| awk '{print $1}' | cut -d":" -f1`
+    do
+	if ! grep $YEAR $file > /dev/null
+        then
+		sed "s/${OLD_YEAR}/${YEAR}/" $file > $file.tmp && mv $file.tmp $file
+		chmod 755 $file
+	fi
+    done
+
