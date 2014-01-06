9555d42ac86875913cba2b63bf1f27f2f31fe93e
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2014-01-06 11:03:14 +0500

    Updated copyright year.

--- StackBuilderPlus/scripts/solaris/launchSBPUpdateMonitor.sh
+++ StackBuilderPlus/scripts/solaris/launchSBPUpdateMonitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2014, EnterpriseDB Corporation.  All rights reserved
 
 export LD_LIBRARY_PATH=INSTALL_DIR/lib:$LD_LIBRARY_PATH
 nohup INSTALL_DIR/bin/UpdateManager --execute "INSTALL_DIR/scripts/launchStackBuilderPlus.sh" > /dev/null 2>&1 &
