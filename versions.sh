8f7c9ae8eaeed8f8512c25f77cd3006ba5e89623
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2014-01-13 15:27:33 +0530

    fixes #32213. Bundle third party headers in the Server installer of Windows-x64 like we do in Windows 32bit installer. Also, bump the build number.

--- versions.sh
+++ versions.sh
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT='V46_0_STABLE'
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.2
-PG_MINOR_VERSION=6.2
+PG_MINOR_VERSION=6.3
 
 # Other package versions
 PG_VERSION_APACHE=2.2.22
