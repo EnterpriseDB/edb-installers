4db345e6629c4019d565acf53e1e4d0b73c69efc
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2014-01-24 12:58:15 +0530

    Bump build number for OpenSSL update

--- versions.sh
+++ versions.sh
@@ -32,7 +32,7 @@ PG_TAG_MIGRATIONTOOLKIT='AS84R1-SP1_84518'
 #                     Minor version is revision.build. 
 
 PG_MAJOR_VERSION=8.4
-PG_MINOR_VERSION=19.1
+PG_MINOR_VERSION=19.2
 
 # Other package versions
 PG_VERSION_POSTGIS=1.4.2
