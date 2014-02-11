5bffc670810ecbd72f804873a6f1d278d46a49d0
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2014-02-11 17:06:46 +0530

    refs #30529. Removed the components that are not specific to 9.2 branch and are being built on 9.3.

--- phpPgAdmin/scripts/linux/launchPhpPgAdmin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012, EnterpriseDB Corporation.  All rights reserved
-
-PORT=`grep APACHE_PORT /etc/postgres-reg.ini | cut -f 2 -d "="`
-URL=http://localhost:$PORT/phpPgAdmin
-INSTALLDIR="INSTALL_DIR"
-
-"$INSTALLDIR/phpPgAdmin/scripts/launchbrowser.sh" $URL
-
