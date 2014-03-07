74bc76ac5b63f68da578abaa55855f60b88a65d1
Bandaru Muralikrishna <bandaru.muralikrishna@enterprisedb.com>
2014-03-07 17:25:16 +0530

    Given precise remote location for Daily builds.

--- autobuild.sh
+++ autobuild.sh
@@ -219,7 +219,7 @@ remote_location="/var/www/html/builds/DailyBuilds/Installers/PG"
 # Different location for the manual and cron triggered builds.
 if [ "$BUILD_USER" == "" ]
 then
-        remote_location="$remote_location/Latest/9.3/PG"
+        remote_location="$remote_location/Latest/9.3"
 else
         remote_location="$remote_location/Custom/$BUILD_USER/9.3/$BUILD_NUMBER"
 fi
@@ -227,7 +227,7 @@ fi
 if [ "$BUILD_USER" == "" ]
 then
         # Get the date of the last successful build (LSB), create the directory of that date and copy the installers from the Latest and copy them to this directory.
-        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-93.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.3/PG; cp $remote_location/* $remote_location/../../../\$LSB_DATE/9.3/PG"
+        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-93.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../\$LSB_DATE/9.3; cp $remote_location/* $remote_location/../../\$LSB_DATE/9.3"
 fi
 
 # Create a remote directory if not present
