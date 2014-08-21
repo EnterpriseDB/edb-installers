207286390bacb46b1b5023a19bdeb3a3368e0d9b
Bandaru Muralikrishna <bandaru.muralikrishna@enterprisedb.com>
2014-08-21 12:49:07 +0530

    Updated MTK license version to 2.8

--- MigrationToolKit/build-osx.sh
+++ MigrationToolKit/build-osx.sh
@@ -26,7 +26,7 @@ _prep_MigrationToolKit_osx() {
     cp -R EDB-MTK/* migrationtoolkit.osx || _die "Failed to copy the source code (source/migrationtoolkit-$PG_VERSION_MIGRATIONTOOLKIT)"
 
     cp pgJDBC-$PG_VERSION_PGJDBC/postgresql-$PG_VERSION_PGJDBC.jdbc4.jar migrationtoolkit.osx/lib/ || _die "Failed to copy the pg-jdbc driver"
-    cp $WD/tarballs/edb-jdbc14.jar migrationtoolkit.osx/lib/ || _die "Failed to copy the edb-jdbc driver"
+    cp $WD/tarballs/edb-jdbc17.jar migrationtoolkit.osx/lib/ || _die "Failed to copy the edb-jdbc driver"
 
     # Remove any existing staging directory that might exist, and create a clean one
     if [ -e $WD/MigrationToolKit/staging/osx ];
