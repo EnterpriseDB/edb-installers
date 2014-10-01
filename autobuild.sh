b0f65499cc0658a5a89ba131d9d4c26d29a8f88c
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2014-10-01 17:23:27 +0530

    Keep only last 5 builds for PEM  on the builds server. Also, place the latest build in the directory Latest.

--- autobuild.sh
+++ autobuild.sh
@@ -219,30 +219,35 @@ echo "Running the build (REL-9_4) " >> autobuild.log
 _mail_status "build-94.log" "build-pvt.log" "9.4"
 
 remote_location="/var/www/html/builds/DailyBuilds/Installers/PG"
+pem_remote_location="/var/www/html/builds/DailyBuilds/Installers/PEM"
 
 echo "Purging old builds from the builds server" >> autobuild.log
 ssh buildfarm@builds.enterprisedb.com "bin/culldirs \"$remote_location/20*\" 5" >> autobuild.log 2>&1
+ssh buildfarm@builds.enterprisedb.com "bin/culldirs \"$pem_remote_location/20*\" 5" >> autobuild.log 2>&1
 
 # Different location for the manual and cron triggered builds.
 if [ "$BUILD_USER" == "" ]
 then
         remote_location="$remote_location/Latest/9.4"
+	pem_remote_location="$pem_remote_location/Latest"
 else
         remote_location="$remote_location/Custom/$BUILD_USER/9.4/$BUILD_NUMBER"
+	pem_remote_location="$pem_remote_location/Custom/$BUILD_USER/$BUILD_NUMBER"
 fi
 
 if [ "$BUILD_USER" == "" ]
 then
         # Get the date of the last successful build (LSB), create the directory of that date and copy the installers from the Latest and copy them to this directory.
         ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-94.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../\$LSB_DATE/9.4; cp $remote_location/* $remote_location/../../\$LSB_DATE/9.4"
+        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $pem_remote_location/build-pvt.log | awk '{print \$6}'\`; mkdir -p $pem_remote_location/../../\$LSB_DATE; cp $pem_remote_location/* $pem_remote_location/../../\$LSB_DATE"
 fi
 
 # Create a remote directory if not present
 echo "Creating $remote_location on the builds server" >> autobuild.log
-ssh buildfarm@builds.enterprisedb.com mkdir -p $remote_location $remote_location/../../../PEM/$DATE >> autobuild.log 2>&1
+ssh buildfarm@builds.enterprisedb.com mkdir -p $remote_location $pem_remote_location >> autobuild.log 2>&1
 
 echo "Uploading pem installers to /var/www/html/builds/DailyBuilds/Installers/PEM/$DATE  on the builds server" >> autobuild.log
-rsync -avh --del output/{pem*,sqlprof*,build-pvt*,php_edbpem*} buildfarm@builds.enterprisedb.com:$remote_location/../../../PEM/$DATE >> autobuild.log 2>&1
+rsync -avh --del output/{pem*,sqlprof*,build-pvt*,php_edbpem*} buildfarm@builds.enterprisedb.com:$pem_remote_location/ >> autobuild.log 2>&1
 
 echo "Uploading output to $remote_location on the builds server" >> autobuild.log
 rsync -avh --del --exclude={pem*,sqlprof*,build-pvt*,server.img,php_edbpem*} output/ buildfarm@builds.enterprisedb.com:$remote_location/ >> autobuild.log 2>&1
