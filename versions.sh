598eb65c54824ecd1f1d57329d5a924faffd3a37
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2014-11-18 09:45:30 +0530

    Update PG version to 9.4rc1

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=9.4beta3
+PG_TARBALL_POSTGRESQL=9.4rc1
 PG_TARBALL_PGADMIN=1.20.0-beta2
 PG_TARBALL_DEBUGGER=0.93
 PG_TARBALL_PLJAVA=1.4.0
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.4
-PG_MINOR_VERSION=0.beta3-2
+PG_MINOR_VERSION=0.rc1
 
 # Other package versions
 PG_VERSION_APACHE=2.4.10
