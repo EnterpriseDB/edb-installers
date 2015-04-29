68a6ffc87968e2e1330234e95395f0cde5e680e0
Bandaru Muralikrishna <bandaru.muralikrishna@enterprisedb.com>
2015-04-29 13:08:58 +0530

    fixes #31040,fixes #33551. The filenames of the PostGIS PNG image files are same for all versions. To distinguish between these, the PostgreSQL version is added in the filenames.

--- PostGIS/build-linux.sh
+++ PostGIS/build-linux.sh
@@ -271,14 +271,17 @@ _postprocess_PostGIS_linux() {
     mkdir -p staging/linux/installer/xdg || _die "Failed to create a directory for the xdg scripts"
     cp -R $WD/scripts/xdg/xdg* staging/linux/installer/xdg || _die "Failed to copy the xdg scripts (scripts/xdg/*)"
     chmod ugo+x staging/linux/installer/xdg/xdg*
-
-    # Copy in the menu pick images and XDG items
-    mkdir -p staging/linux/scripts/images || _die "Failed to create a directory for the menu pick images"
-    cp resources/*.png staging/linux/scripts/images || _die "Failed to copy the menu pick images (resources/*.png)"
-
+ 
     # Version string, for the xdg filenames
     PG_VERSION_STR=`echo $PG_MAJOR_VERSION | sed 's/\./_/g'`
     POSTGIS_VERSION_STR=`echo $PG_VERSION_POSTGIS | cut -f1,2 -d "." | sed 's/\./_/g'`
+
+    # Copy in the menu pick images and XDG items
+    mkdir -p staging/linux/scripts/images || _die "Failed to create a directory for the menu pick images"
+    cp resources/pg-postgresql.png staging/linux/scripts/images/pg-postgresql-$PG_VERSION_STR.png || _die "Failed to copy a menu pick png"
+    cp resources/pg-postgis.png staging/linux/scripts/images/pg-postgis-$POSTGIS_VERSION_STR-$PG_VERSION_STR.png || _die "Failed to copy a menu pick png"
+    cp resources/pg-launchPostGISDocs.png staging/linux/scripts/images/pg-launchPostGISDocs-$POSTGIS_VERSION_STR-$PG_VERSION_STR.png || _die "Failed to copy a menu pick image"
+    cp resources/pg-launchPostGISJDBCDocs.png staging/linux/scripts/images/pg-launchPostGISJDBCDocs-$POSTGIS_VERSION_STR-$PG_VERSION_STR.png || _die "Failed to copy a menu pick imgae"
 
     mkdir -p staging/linux/scripts/xdg || _die "Failed to create a directory for the menu pick items"
     cp resources/xdg/pg-postgresql.directory staging/linux/scripts/xdg/pg-postgresql-$PG_VERSION_STR.directory || _die "Failed to copy a menu pick directory"
