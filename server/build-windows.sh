7426884453e9c9f27806d497de44f51e126daee7
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2015-07-03 16:47:16 +0530

    fixes #35408. Remove the test modules from the staging on Windows

--- server/build-windows.sh
+++ server/build-windows.sh
@@ -439,6 +439,12 @@ EOT
     unzip $WD/server/staging/windows/output.zip -d $WD/server/staging/windows/ || _die "Failed to unpack the built source tree ($WD/staging/windows/output.zip)"
     rm $WD/server/staging/windows/output.zip
 
+    # fixes #35408. In 9.5, some modules were moved from contrib to src/test/modules. They are meant for server testing
+    # and should not be packaged for distribution. On Unix, the top level make does not build these, but on windows it does.
+    # Hence, removing the files of these modules from the staging
+    find $WD/server/staging/windows/ -type f \( -name "test_parser*" -o -name "test_shm_mq*" -o -name "test_decoding*" -o -name "test_ddl_deparse*" \
+                                               -o -name "test_rls_hooks*" -o -name "worker_spi*" -o -name "dummy_seclabel*" \) -exec rm {} \;
+
     # sign stackbuilder
     win32_sign "stackbuilder.exe" "$WD/server/staging/windows/bin"
 
@@ -455,7 +461,6 @@ EOT
     cd $WD
     echo "END BUILD Server Windows"
 }
-
 
 ################################################################################
 # Post process
