403002618f6118db860faad64d9f4da110d2941c
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2015-09-23 12:41:40 +0530

    Add location to the dailybuild path on builds to distinguish between primary and backup build systems (For Sagar)

--- autobuild.sh
+++ autobuild.sh
@@ -255,7 +255,7 @@ fi
 if [ "$BUILD_USER" == "" ]
 then
         # Get the date of the last successful build (LSB), create the directory of that date and copy the installers from the Latest and copy them to this directory.
-        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-95.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../\$LSB_DATE/9.5; cp $remote_location/* $remote_location/../../\$LSB_DATE/9.5"
+        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-95.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.5/$country; cp $remote_location/* $remote_location/../../../\$LSB_DATE/9.5/$country"
 fi
 
 # Create a remote directory if not present
