97703239da9928573b1bb3cb001a30acc7c383b8
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2015-10-12 11:52:21 +0100

    Reverting mod_wsgi changes in ApachePhp.

--- ApachePhp/scripts/windows/install-apache.bat
+++ ApachePhp/scripts/windows/install-apache.bat
@@ -8,10 +8,5 @@ echo       then it will not run successfully.
 rem Go to the directory from where it has been called
 
 rem Install the server
-
-SET PYTHONHOME="@@LP_PYTHON_HOME@@"
-SET PYTHONPATH="@@LP_PYTHON_HOME@@"
-SET PATH="@@LP_PYTHON_HOME@@";%PATH%
-
 "@@APACHE_HOME@@\bin\httpd.exe" -k install -n "EnterpriseDB ApachePHP"
 
--- ApachePhp/scripts/windows/start-apache.bat
+++ ApachePhp/scripts/windows/start-apache.bat
@@ -5,10 +5,5 @@ echo       or from the Administrator console.
 echo       If you have not started this as a administrator,
 echo       then it will not run successfully.
 
-
-SET PYTHONHOME="@@LP_PYTHON_HOME@@"
-SET PYTHONPATH="@@LP_PYTHON_HOME@@"
-SET PATH="@@LP_PYTHON_HOME@@";%PATH%
-
 "@@APACHE_HOME@@\bin\httpd.exe" -k start -n "EnterpriseDB ApachePHP"
 
