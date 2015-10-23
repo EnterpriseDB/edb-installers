830d19209426343dd68c0e9d20d1ec44e36a8e6d
Sagar Yedida <sagar.yedida@enterprisedb.com>
2015-10-23 17:37:53 +0530

    Remove 9.0 from Autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -61,46 +61,6 @@ DATE=`date +'%Y-%m-%d'`
 echo "Cleaning up old output" >> autobuild.log
 rm -rf output/* >> autobuild.log 2>&1
 
-# Switch to REL-9_0 branch
-echo "Switching to REL-9_0 branch" >> autobuild.log
-/opt/local/bin/git reset --hard >> autobuild.log 2>&1
-/opt/local/bin/git checkout REL-9_0 >> autobuild.log 2>&1
-
-# Self update
-echo "Updating build system" >> autobuild.log
-/opt/local/bin/git reset --hard >> autobuild.log 2>&1
-/opt/local/bin/git pull >> autobuild.log 2>&1
-
-# Make sure, we always do a full build
-if [ -f settings.sh.full.REL-9_0 ]; then
-   cp -f settings.sh.full.REL-9_0 settings.sh
-fi
-
-# Run the build, and dump the output to a log file
-echo "Running the build" >> autobuild.log
-./build.sh > output/build-90.log 2>&1
-
-_mail_status "build-90.log" "9.0"
-
-remote_location="/var/www/html/builds/DailyBuilds/Installers/PG"
-
-echo "Purging old builds from the builds server" >> autobuild.log
-ssh buildfarm@builds.enterprisedb.com "bin/culldirs "$remote_location/20*" 5" >> autobuild.log 2>&1
-
-# remote location
-remote_location_90="$remote_location/$DATE/9.0"
-
-# Create a remote directory and upload the output.
-echo "Creating $remote_location_90 on the builds server" >> autobuild.log
-ssh buildfarm@builds.enterprisedb.com mkdir -p $remote_location_90 >> autobuild.log 2>&1
-
-echo "Uploading output to $remote_location_90 on the builds server" >> autobuild.log
-scp output/* buildfarm@builds.enterprisedb.com:$remote_location_90 >> autobuild.log 2>&1
-
-# Clear out 9.0 output
-echo "Cleaning up 9.0 output" >> autobuild.log
-rm -rf output/* >> autobuild.log 2>&1
-
 # Switch to REL-9_1 branch
 echo "Switching to REL-9_1 branch" >> autobuild.log
 /opt/local/bin/git reset --hard >> autobuild.log 2>&1
@@ -121,8 +81,9 @@ echo "Running the build (REL-9_1) " >> autobuild.log
 
 _mail_status "build-91.log" "9.1"
 
-# remote location
-# create directory with the server country name
+remote_location="/var/www/html/builds/DailyBuilds/Installers/PG"
+
+# Determine the host location
 dns=$(grep -w "172.24" /etc/resolv.conf | cut -f3 -d".") >> autobuild.log 2>&1
 
 if [ $dns -eq 32 ]
@@ -142,6 +103,9 @@ fi
 
 echo "Host country = $country" >> autobuild.log
 remote_location_91="$remote_location/$DATE/9.1/$country"
+
+echo "Purging old builds from the builds server" >> autobuild.log
+ssh buildfarm@builds.enterprisedb.com "bin/culldirs "$remote_location/20*" 5" >> autobuild.log 2>&1
 
 # Create a remote directory and upload the output.
 echo "Creating $remote_location_91 on the builds server" >> autobuild.log
