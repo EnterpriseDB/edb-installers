4fc75851c23086b9d8360eacbd3122f824c127b6
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2015-11-10 11:31:22 +0000

    RM#36369: Added systemd service support for ApacheHTTPD installer on RHEL7 platform.

--- ApacheHTTPD/scripts/linux/startupcfg.sh
+++ ApacheHTTPD/scripts/linux/startupcfg.sh
@@ -2,13 +2,14 @@
 # Copyright (c) 2012-2015, EnterpriseDB Corporation.  All rights reserved
 
 # Check the command line
-if [ $# -ne 1 ]; 
+if [ $# -ne 2 ]; 
 then
-    echo "Usage: $0 <Install dir> "
+    echo "Usage: $0 <Install dir> <initSystem>"
     exit 127
 fi
 
 INSTALLDIR=$1
+INIT=$2
 
 # Exit code
 WARN=0
@@ -24,6 +25,42 @@ _warn() {
     WARN=2
 }
 
+determine_services_path() {
+        echo "==>> Checking systemd services path..."
+        echo "Running: /usr/bin/pkg-config --variable=systemdsystemunitdir systemd"
+        SYSTEMD_SERVICES_PATH=`/usr/bin/pkg-config --variable=systemdsystemunitdir systemd`
+
+        if [ -z $SYSTEMD_SERVICES_PATH ]; then
+            SYSTEMD_SERVICES_PATH=/usr/lib/systemd/system
+        fi
+        echo "SYSTEMD_SERVICES_PATH:$SYSTEMD_SERVICES_PATH"
+}
+
+if [ "$INIT" = "systemd" ]; then
+determine_services_path
+    cat <<EOT > "$SYSTEMD_SERVICES_PATH/EnterpriseDBApacheHTTPD.service"
+[Unit]
+Description=Starts and stops the Apache Server
+After=syslog.target network.target
+
+[Service]
+TimeoutSec=20
+Type=forking
+SuccessExitStatus=1
+StandardError=syslog
+
+Environment=LD_LIBRARY_PATH=$INSTALLDIR/lib:\$LD_LIBRARY_PATH
+ExecStart=$INSTALLDIR/apache/bin/apachectl start
+
+[Install]
+WantedBy=multi-user.target
+
+EOT
+
+$SYSTEMD_PATH/bin/systemctl daemon-reload
+$SYSTEMD_PATH/bin/systemctl enable EnterpriseDBApacheHTTPD.service
+
+else
 # Write the startup script
 cat <<EOT > "/etc/init.d/EnterpriseDBApacheHTTPD"
 #!/bin/bash
@@ -103,5 +140,6 @@ then
     fi
 fi
 
+fi
 echo "$0 ran to completion"
 exit $WARN
