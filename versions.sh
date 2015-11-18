c9771e20d5f5bf198437ceaa6180eb4c4d5d80e6
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2015-11-18 17:16:58 +0530

    fixes #36432, refs #32485
    As part of fix for 32485, the Base Directory path in the registry contains the forward slashes
    instead of backward slashes because of the absolutize action of bitrock installbuilder. This
    makes the installation directory validation check to fail.
    Revert that fix and add the fix to correct the slashes in the Base Directory key value.

--- versions.sh
+++ versions.sh
@@ -36,7 +36,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.4
-PG_MINOR_VERSION=5.1
+PG_MINOR_VERSION=5.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.16
