b3a2ccf18f5e371254c7769c07f481fafd0e14d4
Muhammad Aqeel <muhammad.aqeel@enterprisedb.com>
2015-12-09 10:41:28 +0000

    Improved search pattern in settings.ini to work with future versions.

--- list_pgadmin_files.sh
+++ list_pgadmin_files.sh
@@ -1,1 +1,1 @@
-find * -type f -exec grep -H 'pgAdmin III' {} \; | grep "settings.ini" | cut -f1 -d":" | rev | cut -f1 -d'/' | rev
+find * -type f -exec grep -H 'pgAdmin .* - PostgreSQL Tools' {} \; | grep "settings.ini" | cut -f1 -d":" | rev | cut -f1 -d'/' | rev
