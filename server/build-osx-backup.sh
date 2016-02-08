b7c571540b50a10d6ea9bc79a6cd668e7012479a
Bandaru Muralikrishna <bandaru.muralikrishna@enterprisedb.com>
2016-02-08 16:45:22 +0530

    fixing typo mistake.

--- server/build-osx-backup.sh
+++ server/build-osx-backup.sh
@@ -429,7 +429,7 @@ _postprocess_server_osx() {
     ssh $PG_SSH_OSX "cd $PG_PATH_OSX/installers; rm -rf postgresql-*.dmg" || _die "Failed to remove the installer from regression installer directory"
 
     # Copy the installer to regression setup
-    ssh $PG_SSH_OSX "cd $PG_PATH_OSX/outputi; cp -p postgresql-*.dmg $PG_PATH_OSX/installers/" || _die "Failed to Copy installer to the regression directory"
+    ssh $PG_SSH_OSX "cd $PG_PATH_OSX/output; cp -p postgresql-*.dmg $PG_PATH_OSX/installers/" || _die "Failed to Copy installer to the regression directory"
 
     # Delete the installer from remote output directory
     ssh $PG_SSH_OSX "cd $PG_PATH_OSX/output; rm -rf postgresql* server* " || _die "Failed to clean the remote output directory"
