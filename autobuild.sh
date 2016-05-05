dd833316ccdec95d869c984d05ad1f9ccc59edd2
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2016-05-05 15:22:18 +0530

    9.6 realted changes in autobuild and installation notes

--- autobuild.sh
+++ autobuild.sh
@@ -17,7 +17,7 @@ usage()
         echo "      [-platforms list]  list of platforms. It may include the list of supported platforms separated by comma or all" 
         echo "      [-packages list]   list of packages. It may include the list of supported platforms separated by comma or all"
         echo "    Examples:"
-        echo "     $BASENAME -skipbuild 0 -platforms "linux, linux_64, windows, windows_x64, osx" -packages "server, apachephp, phppgadmin, pgjdbc, psqlodbc, slony, postgis, npgsql, pgagent, pgmemcache, pgbouncer, migrationtoolkit, sqlprotect, update_monitor""
+        echo "     $BASENAME -skipbuild 0 -platforms "linux, linux_64, windows, windows_x64, osx" -packages "server, apachehttpd, phppgadmin, pgjdbc, psqlodbc, slony, postgis, npgsql, pgagent, pgmemcache, pgbouncer, migrationtoolkit, sqlprotect, update_monitor""
         echo "     $BASENAME -skipbuild 1 -platforms "all" -packages "all""
         echo "     $BASENAME -skipbuild 1 -skippvtpkg 1 -platforms "all" -packages "all""
         echo ""
@@ -68,8 +68,8 @@ then
 else
         SKIPPVTPACKAGES=""
 	# Make sure, we always do a full private build
-	if [ -f pvt_settings.sh.full.REL-9_5 ]; then
-		cp -f pvt_settings.sh.full.REL-9_5 pvt_settings.sh.REL-9_5
+	if [ -f pvt_settings.sh.full.REL-9_6 ]; then
+		cp -f pvt_settings.sh.full.REL-9_6 pvt_settings.sh.REL-9_6
 	fi
 fi
 
@@ -107,7 +107,7 @@ fi
 if ! echo $PACKAGES | grep -w -i all > /dev/null
 then
 _set_config_package SERVER
-_set_config_package APACHEPHP
+_set_config_package APACHEHTTPD
 _set_config_package PHPPGADMIN
 _set_config_package PGJDBC
 _set_config_package PSQLODBC
@@ -196,23 +196,23 @@ DATE=`date +'%Y-%m-%d'`
 echo "Cleaning up old output" >> autobuild.log
 rm -rf output/* >> autobuild.log 2>&1
 
-# Switch to REL-9_5 branch
-echo "Switching to REL-9_5 branch" >> autobuild.log
+# Switch to REL-9_6 branch
+echo "Switching to REL-9_6-pgadmin3 branch" >> autobuild.log
 git reset --hard >> autobuild.log 2>&1
-git checkout REL-9_5 >> autobuild.log 2>&1
+git checkout REL-9_6-pgadmin3 >> autobuild.log 2>&1
 
 # Make sure, we always do a full build
-if [ -f settings.sh.full.REL-9_5 ]; then
-   cp -f settings.sh.full.REL-9_5 settings.sh
+if [ -f settings.sh.full.REL-9_6 ]; then
+   cp -f settings.sh.full.REL-9_6 settings.sh
 fi
 
 # Self update
-echo "Updating REL-9_5 branch build system" >> autobuild.log
+echo "Updating REL-9_6-pgadmin3 branch build system" >> autobuild.log
 git pull >> autobuild.log 2>&1
 
 # Run the build, and dump the output to a log file
-echo "Running the build (REL-9_5) " >> autobuild.log
-./build.sh $SKIPBUILD $SKIPPVTPACKAGES 2>&1 | tee output/build-95.log
+echo "Running the build (REL-9_6) " >> autobuild.log
+./build.sh $SKIPBUILD $SKIPPVTPACKAGES 2>&1 | tee output/build-96.log
 
 remote_location="/var/www/html/builds/DailyBuilds/Installers/PG"
 pem_remote_location="/var/www/html/builds/DailyBuilds/Installers/PEM/v6.0"
@@ -243,19 +243,19 @@ ssh buildfarm@builds.enterprisedb.com "bin/culldirs \"$pem_remote_location/20*\"
 if [ "$BUILD_USER" == "" ]
 then
         echo "Host country = $country" >> autobuild.log
-        remote_location="$remote_location/Latest/9.5/$country"
+        remote_location="$remote_location/Latest/9.6/$country"
         pem_remote_location="$pem_remote_location/$DATE/$country"
 else
-        remote_location="$remote_location/Custom/$BUILD_USER/9.5/$country/$BUILD_NUMBER"
+        remote_location="$remote_location/Custom/$BUILD_USER/9.6/$country/$BUILD_NUMBER"
         pem_remote_location="$pem_remote_location/Custom/$BUILD_USER/$country/$BUILD_NUMBER"
 fi
 
-_mail_status "build-95.log" "build-pvt.log" "9.5"
+_mail_status "build-96.log" "build-pvt.log" "9.6"
 
 if [ "$BUILD_USER" == "" ]
 then
         # Get the date of the last successful build (LSB), create the directory of that date and copy the installers from the Latest and copy them to this directory.
-        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-95.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.5/$country; cp $remote_location/* $remote_location/../../../\$LSB_DATE/9.5/$country"
+        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-96.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.6/$country; cp $remote_location/* $remote_location/../../../\$LSB_DATE/9.6/$country"
 fi
 
 # Create a remote directory if not present
--- pvt_build.sh
+++ pvt_build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -e $WD/pvt_settings.sh.REL-9_5 ]
+if [ -e $WD/pvt_settings.sh.REL-9_6 ]
 then
 
     # Source the private settings.    
-    source $WD/pvt_settings.sh.REL-9_5
+    source $WD/pvt_settings.sh.REL-9_6
 
     # Create the pvt_packages directory. If not exist.
     if [ ! -e $WD/pvt_packages ];
