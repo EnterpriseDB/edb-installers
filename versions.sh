6f7bc0780ec3aad857cdd92ddf5702321d10d22a
Bandaru Muralikrishna <bandaru.muralikrishna@enterprisedb.com>
2016-05-28 18:52:59 +0530

    Adding -O2 in CLFAGS, Bumping server build number.

--- versions.sh
+++ versions.sh
@@ -33,7 +33,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.5
-PG_MINOR_VERSION=3.1
+PG_MINOR_VERSION=3.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.20
