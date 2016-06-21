e9f040cd0c0345b4ca47422aa76623bc9f2e784d
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2016-06-21 21:10:00 +0530

    set ld_library_path for psycopg2 to load its dependencies

--- server/scripts/linux/launchpgadmin.sh
+++ server/scripts/linux/launchpgadmin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Copyright (c) 2012-2016, EnterpriseDB Corporation.  All rights reserved
 
-"PG_INSTALLDIR/pgAdmin 4/bin/pgadmin4"
+LD_LIBRARY_PATH="PG_INSTALLDIR/lib:PG_INSTALLDIR/pgAdmin 4/venv/lib:\$LD_LIBRARY_PATH" "PG_INSTALLDIR/pgAdmin 4/bin/pgadmin4"
 
 
