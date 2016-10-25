16f15e1e6b788c3f7d0c6b59b4e7e6f9d1326969
Srinu <srinu@laptop131.pn.in>
2016-10-25 12:09:35 +0530

    Set PostgreSQL Version to 9.5.5

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=9.5.4
+PG_TARBALL_POSTGRESQL=9.5.5
 PG_TARBALL_PGADMIN=1.22.1
 PG_TARBALL_DEBUGGER=0.93
 PG_TARBALL_PLJAVA=1.4.0
@@ -35,7 +35,7 @@ PG_TAG_HDFS_FDW=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.5
-PG_MINOR_VERSION=4.3
+PG_MINOR_VERSION=5.1
 
 # Other package versions
 PG_VERSION_APACHE=2.4.23
