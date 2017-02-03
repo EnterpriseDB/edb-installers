ba9fbbadd1138c4c7813ccc6ab0dd20b153b03d5
Srinu <srinu@laptop131.pn.in>
2017-02-03 18:34:31 +0530

    fixes 39668. Updated Copy Right Year to 2017 for all components.

--- server/scripts/linux/launchpgadmin.sh
+++ server/scripts/linux/launchpgadmin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2012-2016, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2017, EnterpriseDB Corporation.  All rights reserved
 
 LD_LIBRARY_PATH="PG_INSTALLDIR/lib:PG_INSTALLDIR/pgAdmin 4/venv/lib:PG_INSTALLDIR/pgAdmin 4/lib:PG_INSTALLDIR/stackbuilder/lib:$LD_LIBRARY_PATH" "PG_INSTALLDIR/pgAdmin 4/bin/pgAdmin4"
 
--- server/scripts/osx/getlocales.sh
+++ server/scripts/osx/getlocales.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2012-2016, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2017, EnterpriseDB Corporation.  All rights reserved
 
 # PostgreSQL server locales script for OSX (returns valid locales on the system)
 
