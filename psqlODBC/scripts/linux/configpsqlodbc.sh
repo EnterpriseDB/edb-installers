12449cb3a20fcdc51ff3a0096b772f4187c69af4
Srinu <srinu@laptop131.pn.in>
2017-02-03 18:34:31 +0530

    fixes 39668. Updated Copy Right Year to 2017 for all components.

--- psqlODBC/scripts/linux/configpsqlodbc.sh
+++ psqlODBC/scripts/linux/configpsqlodbc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012-2016, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2017, EnterpriseDB Corporation.  All rights reserved
 
 # Check the command line
 if [ $# -ne 1 ];
