bf036b6bb30e85167e71044ad5b6cab4c17e3947
Srinu Perabattula <srinu.perabattula@enterprisedb.com>
2017-02-20 17:52:51 +0530

    Bumping build number for Bitrock InstallBuilder update

--- versions.sh
+++ versions.sh
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.6
-PG_MINOR_VERSION=2.1
+PG_MINOR_VERSION=2.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.25
