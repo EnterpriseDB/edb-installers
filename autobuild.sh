6e9b2deb64eec19b574b2cf029c18eb36ae74652
Apoorva Kailaswar <apoorva.kailaswar@enterprisedb.com>
2017-03-29 13:16:33 +0530

    Changes in autobuild script:  - use -R option with cp so that symbols and 3rd party directories are also get copied

--- autobuild.sh
+++ autobuild.sh
@@ -254,7 +254,7 @@ _mail_status "build-93.log" "build-pvt.log" "9.3"
 if [ "$BUILD_USER" == "" ]
 then
         # Get the date of the last successful build (LSB), create the directory of that date and copy the installers from the Latest and copy them to this directory.
-        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-93.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.3/$country; cp $remote_location/* $remote_location/../../../\$LSB_DATE/9.3/$country"
+        ssh buildfarm@builds.enterprisedb.com "export LSB_DATE=\`ls -l --time-style=+%Y-%m-%d $remote_location/build-93.log | awk '{print \$6}'\`; mkdir -p $remote_location/../../../\$LSB_DATE/9.3/$country; cp -R $remote_location/* $remote_location/../../../\$LSB_DATE/9.3/$country"
 fi
 
 # Create a remote directory if not present
