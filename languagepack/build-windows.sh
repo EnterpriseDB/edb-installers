6f6435a444335d5accf846d10149134110392546
ZilanBasha.Shaik <zilanbasha.shaik@enterprisedb.com>
2017-04-20 11:52:50 +0530

    In LanguagePack for windows32/64 with skipbuild not clean the existing staging directory which is fixed now Reviewed by Sandeep Thakkar

--- languagepack/build-windows.sh
+++ languagepack/build-windows.sh
@@ -219,6 +219,25 @@ _postprocess_languagepack_windows() {
 
     ssh $PG_SSH_WIN "cd $PG_LANGUAGEPACK_INSTALL_DIR_WIN; rm -f Tcl-8.5.zip Perl-5.20.zip Python-3.3.zip " || _die "Failed to remove  Tcl-8.5.zip;Perl-5.20.zip; Python-3.3.zip on  windows buildhost"
     cd $WD/languagepack/staging/$ARCH/
+    # Remove any existing staging directory that might exist, and create a clean one
+    if [ -e $WD/languagepack/staging/$ARCH/Tcl-8.5 ];
+    then
+       echo "Removing existing staging Tcl-8.5 directory"
+       rm -rf $WD/languagepack/staging/$ARCH/Tcl-8.5 || _die "Failed to remove the Tcl-8.5 directory"
+    fi
+
+    if [ -e $WD/languagepack/staging/$ARCH/Perl-5.20 ];
+    then
+       echo "Removing existing staging Perl-5.20 directory"
+       rm -rf $WD/languagepack/staging/$ARCH/Perl-5.20 || _die "Failed to remove the Perl-5.20 directory"
+    fi
+
+    if [ $WD/languagepack/staging/$ARCH/Python-3.3 ];
+    then
+       echo "Removing existing staging Python-3.3 directory"
+       rm -rf $WD/languagepack/staging/$ARCH/Python-3.3 || _die "Failed to remove the Python-3.3 directory"
+    fi
+
     unzip Tcl-8.5.zip ||_die " ailed to unzip Tcl-8.5.zip"
     unzip Perl-5.20.zip || _die "Failed to unzip Perl-5.20.zip"
     unzip Python-3.3.zip || _die "Failed to unzip Python-3.3.zip"
