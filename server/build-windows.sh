983b3273278691cb35a5c1f09766b0fed352ea03
Paresh More <paresh.more@enterprisedb.com>
2017-05-18 18:55:11 +0530

    Changing typo changes variable was pointing to wrong folder

--- server/build-windows.sh
+++ server/build-windows.sh
@@ -574,7 +574,7 @@ EOT
     echo "Restructuring Stackbuilder"
     mkdir -p $SB_STAGING_WINDOWS/bin || _die "Couldn't create the staging directory $CLT_STAGING_WINDOWS/scripts/images"
     mv $WD/server/staging/windows/bin/stackbuilder.exe $SB_STAGING_WINDOWS/bin || _die "Failed to move stackbuilder.exe"
-    cp  $WD/server/staging/windows/bin/*.dll $SB_STAGING_WINDOWS_X64/bin || _die "Failed to move dlls"
+    cp  $WD/server/staging/windows/bin/*.dll $SB_STAGING_WINDOWS/bin || _die "Failed to move dlls"
     mv $WD/server/staging/windows/StackBuilder $SB_STAGING_WINDOWS || _die "Failed to move stackbuilder.exe"
 
     echo "Restructuring Server"
