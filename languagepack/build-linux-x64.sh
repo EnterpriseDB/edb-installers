b727a6b8cda372a4b714e49826ff80f033815703
Talha Bin Rizwan <talha.rizwan@enterprisedb.com>
2017-07-24 00:51:14 +0500

    Adding changes to contain the last successful staging directory for languagepack.

--- languagepack/build-linux-x64.sh
+++ languagepack/build-linux-x64.sh
@@ -57,6 +57,17 @@ _prep_languagepack_linux_x64() {
 _build_languagepack_linux_x64() {
 
     ssh $PG_SSH_LINUX_X64 "cd $PG_PATH_LINUX_X64/languagepack/source/languagepack.linux-x64; export SSL_INST=/opt/local/Current; ./languagepack.sh -n 5.9 -p 3.3.4 -d 0.6.49 -t 8.5.17 -P 5.16.3 -v 9.4 -i $PG_LANGUAGEPACK_INSTALL_DIR_LINUX" || _die "Failed to build languagepack"
+
+    echo "Removing last successful staging directory ($PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging)"
+    ssh $PG_SSH_LINUX_X64 "rm -rf $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't remove the last successful staging directory directory"
+    ssh $PG_SSH_LINUX_X64 "mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't create the last successful staging directory"
+    ssh $PG_SSH_LINUX_X64 "chmod ugo+w $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't set the permission on the last successful staging directory"
+
+    echo "Copying the complete build to the successful staging directory"
+    ssh $PG_SSH_LINUX_X64 "cp -rp $PG_LANGUAGEPACK_INSTALL_DIR_LINUX/* $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't copy the existing staging directory"
+
+    ssh $PG_SSH_LINUX_X64 "echo PG_VERSION_LANGUAGEPACK=$PG_VERSION_LANGUAGEPACK > $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/versions-linux-x64.sh" || _die "Failed to write languagepack version number into versions-linux-x64.sh"
+    ssh $PG_SSH_LINUX_X64 "echo PG_BUILDNUM_LANGUAGEPACK=$PG_BUILDNUM_LANGUAGEPACK >> $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/versions-linux-x64.sh" || _die "Failed to write languagepack build number into versions-linux-x64.sh"
 }
 
 
@@ -65,11 +76,25 @@ _build_languagepack_linux_x64() {
 ################################################################################
 
 _postprocess_languagepack_linux_x64() {
- 
-    cd $WD/languagepack
+
+    # Remove any existing staging/install directory that might exist, and create a clean one
+    if [ -e $WD/languagepack/staging/linux-x64 ];
+    then
+      echo "Removing existing staging directory"
+      rm -rf $WD/languagepack/staging/linux-x64 || _die "Couldn't remove the existing staging directory"
+    fi
+    echo "Creating staging/install directory ($WD/languagepack/staging/linux-x64)"
+    mkdir -p $WD/languagepack/staging/linux-x64 || _die "Couldn't create the staging directory"
+    chmod ugo+w $WD/languagepack/staging/linux-x64 || _die "Couldn't set the permissions on the staging directory"
 
     echo "Copying files to staging directory from install directory"
-    ssh $PG_SSH_LINUX_X64 "mv $PG_LANGUAGEPACK_INSTALL_DIR_LINUX/$PG_VERSION_LANGUAGEPACK/* $PG_PATH_LINUX_X64/languagepack/staging/linux-x64 && rm -rf $PG_LANGUAGEPACK_INSTALL_DIR_LINUX" || _die "Failed to copy the languagepack Source into the staging directory"
+    ssh $PG_SSH_LINUX_X64 "cp -rp $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/* $PG_PATH_LINUX_X64/languagepack/staging/linux-x64" || _die "Failed to copy the languagepack Source into the staging directory"
+
+    source $WD/languagepack/staging/linux-x64/versions-linux-x64.sh
+    PG_BUILD_LANGUAGEPACK=$(expr $PG_BUILD_LANGUAGEPACK + $SKIPBUILD)
+
+    cd $WD/languagepack
+
 
     pushd staging/linux-x64
     generate_3rd_party_license "languagepack"
@@ -77,6 +102,16 @@ _postprocess_languagepack_linux_x64() {
     
     # Build the installer
     "$PG_INSTALLBUILDER_BIN" build installer.xml linux-x64 || _die "Failed to build the installer"
+
+    # If build passed empty this variable
+    BUILD_FAILED="build_failed-"
+    if [ $PG_BUILD_LANGUAGEPACK -gt 0 ];
+    then
+        BUILD_FAILED=""
+    fi
+
+    # Rename the installer
+    mv $WD/output/edb_languagepack-$PG_VERSION_LANGUAGEPACK-$PG_BUILDNUM_LANGUAGEPACK-linux-x64.run $WD/output/edb_languagepack-$PG_VERSION_LANGUAGEPACK-$PG_BUILDNUM_LANGUAGEPACK-${BUILD_FAILED}linux-x64.run
 
     cd $WD
 }
--- languagepack/build-linux.sh
+++ languagepack/build-linux.sh
@@ -58,6 +58,18 @@ _build_languagepack_linux() {
 
     ssh $PG_SSH_LINUX "cd $PG_PATH_LINUX/languagepack/source/languagepack.linux; export SSL_INST=/opt/local/Current; ./languagepack.sh -n 5.9 -p 3.3.4 -d 0.6.49 -t 8.5.17 -P 5.16.3 -v 9.4 -i $PG_LANGUAGEPACK_INSTALL_DIR_LINUX" || _die "Failed to build languagepack"
     #ssh $PG_SSH_LINUX "cd $PG_PATH_LINUX/languagepack/source/languagepack.linux; export SSL_INST=/opt/local/Current; ./languagepack.sh -n 5.9 -p 3.3.4 -d 0.6.49 -t 8.5.17 -P 5.16.3 -v 9.4 -i $PG_LANGUAGEPACK_INSTALL_DIR_LINUX" || _die "Failed to build languagepack"
+
+    echo "Removing last successful staging directory ($PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging)"
+    ssh $PG_SSH_LINUX "rm -rf $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't remove the last successful staging directory directory"
+    ssh $PG_SSH_LINUX "mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't create the last successful staging directory"
+    ssh $PG_SSH_LINUX "chmod ugo+w $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't set the permission on the last successful staging directory"
+
+    echo "Copying the complete build to the successful staging directory"
+    ssh $PG_SSH_LINUX "cp -rp $PG_LANGUAGEPACK_INSTALL_DIR_LINUX/* $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging" || _die "Couldn't copy the existing staging directory"
+
+    ssh $PG_SSH_LINUX "echo PG_VERSION_LANGUAGEPACK=$PG_VERSION_LANGUAGEPACK > $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/versions-linux.sh" || _die "Failed to write languagepack version number into versions-linux.sh"
+    ssh $PG_SSH_LINUX "echo PG_BUILDNUM_LANGUAGEPACK=$PG_BUILDNUM_LANGUAGEPACK >> $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/versions-linux.sh" || _die "Failed to write languagepack build number into versions-linux.sh"
+
 }
 
 
@@ -66,12 +78,24 @@ _build_languagepack_linux() {
 ################################################################################
 
 _postprocess_languagepack_linux() {
- 
-    cd $WD/languagepack
+
+    # Remove any existing staging directory that might exist, and create a clean one
+    if [ -e "$WD/languagepack/staging/linux" ];
+    then
+      echo "Removing existing staging directory"
+      rm -rf $WD/languagepack/staging/linux || _die "Couldn't remove the existing staging directory"
+    fi
+    echo "Creating staging directory ($WD/languagepack/staging/linux)"
+    mkdir -p $WD/languagepack/staging/linux || _die "Couldn't create the staging directory"
+    chmod ugo+w $WD/languagepack/staging/linux || _die "Couldn't set the permissions on the staging directory"
+
+    source $WD/languagepack/staging/linux/versions-linux.sh
+    PG_BUILD_LANGUAGEPACK=$(expr $PG_BUILD_LANGUAGEPACK + $SKIPBUILD)
 
     echo "Copying files to staging directory from install directory"
-    ssh $PG_SSH_LINUX "mv $PG_LANGUAGEPACK_INSTALL_DIR_LINUX/$PG_VERSION_LANGUAGEPACK/* $PG_PATH_LINUX/languagepack/staging/linux && rm -rf $PG_LANGUAGEPACK_INSTALL_DIR_LINUX" || _die "Failed
- to copy the languagepack Source into the staging directory"
+    ssh $PG_SSH_LINUX "cp -rp $PG_LANGUAGEPACK_INSTALL_DIR_LINUX.staging/* $PG_PATH_LINUX/languagepack/staging/linux" || _die "Failed to copy the built tree into the staging directory"
+
+    cd $WD/languagepack
 
     pushd staging/linux
     generate_3rd_party_license "languagepack"
@@ -79,6 +103,16 @@ _postprocess_languagepack_linux() {
 
     # Build the installer
     "$PG_INSTALLBUILDER_BIN" build installer.xml linux || _die "Failed to build the installer"
+
+    # If build passed empty this variable
+    BUILD_FAILED="build_failed-"
+    if [ $PG_BUILD_LANGUAGEPACK -gt 0 ];
+    then
+        BUILD_FAILED=""
+    fi
+
+    # Rename the installer
+    mv $WD/output/edb_languagepack-$PG_VERSION_LANGUAGEPACK-$PG_BUILDNUM_LANGUAGEPACK-linux.run $WD/output/edb_languagepack-$PG_VERSION_LANGUAGEPACK-$PG_BUILDNUM_LANGUAGEPACK-${BUILD_FAILED}linux.run
 
     cd $WD
 }
