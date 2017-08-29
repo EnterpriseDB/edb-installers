e075289a2911d8709c6d302df3ec5b6911a4b51b
Srinu Perabattula <srinu.perabattula@enterprisedb.com>
2017-08-29 07:57:35 +0530

    Set PostgreSQL version to beta4

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=10beta3
+PG_TARBALL_POSTGRESQL=10beta4
 PG_TARBALL_PGADMIN=1.6
 PG_TARBALL_DEBUGGER=0.93
 PG_TARBALL_PLJAVA=1.4.0
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=10
-PG_MINOR_VERSION=0.beta3
+PG_MINOR_VERSION=0.beta4
 
 # Other package versions
 PG_VERSION_APACHE=2.4.25
