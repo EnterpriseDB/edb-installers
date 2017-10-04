2277e010dc8a273c25872fa13c6d6b2078efdf3a
ZilanBasha.Shaik <zilanbasha.shaik@enterprisedb.com>
2017-10-04 19:00:20 +0530

    Refs #38080. Added the checkDirPermissions.sh to check the permissions of a directory.

---
+++ scripts/common_scripts/linux/checkDirPermissions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2012-2017, EnterpriseDB Corporation.  All rights reserved
+# This script accepts a path, which it traverses and checks if 'others' users have
+# read & execute permissions. If any of the directory do not have the permissions
+# then it will echo "100" and exit.
+
+parts=`echo $1 | awk 'BEGIN{FS="/"}{for (i=1; i < NF; i++) print $i}'`
+
+for part in $parts
+do
+        path="$path/$part"
+	if [ -d $path ]; then
+                if [ `ls -ld $path | awk '{print $1}' | awk '{ print substr( $0, 8) }' | grep r` ] && [[ `ls -ld $path | awk '{print $1}' | awk '{ print substr( $0, 8) }' | grep x` || `ls -ld $path | awk '{print $1}' | awk '{ print substr( $0, 8) }' | grep t` ]]; then
+                        :
+                        #echo "read & execute permissions for others to $path \n"
+                else
+                        #echo "No read & execute permissions for others to $path \n"
+                        echo 100
+                        exit
+                fi
+        fi
+done
