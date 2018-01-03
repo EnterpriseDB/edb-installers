63f3429036e0a7ef8544617c0d392fd13a5f33fc
Srinu Perabattula <srinu.perabattula@enterprisedb.com>
2018-01-03 16:55:25 +0530

    Updating copyright year to 2018 for all the components

--- PEM-HTTPD/scripts/windows/stopApacheService.bat
+++ PEM-HTTPD/scripts/windows/stopApacheService.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-REM Copyright (c) 2012-2017, EnterpriseDB Corporation.  All rights reserved
+REM Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
 rem Stoping the EnterpriseDBPEMHTTPDService
 sc stop "PEMHTTPD"
