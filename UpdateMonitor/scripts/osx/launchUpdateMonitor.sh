66ce8b15cc017eb84352379528944c00c7a74de8
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2018-03-19 14:15:14 +0530

    Remove components that are either unsupported or being built on latest stable branch

--- UpdateMonitor/scripts/osx/launchUpdateMonitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
-
-cd INSTALL_DIR/UpdateMonitor.app/Contents/MacOS
-INSTALL_DIR/UpdateMonitor.app/Contents/MacOS/UpdateManager --execute "/Applications/BRANDING/Application Stack Builder.app/Contents/MacOS/applet" &
-
--- UpdateMonitor/scripts/solaris/launchUpdateMonitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
-
-export LD_LIBRARY_PATH=INSTALL_DIR/lib:STACKBUILDER_DIR/lib:$LD_LIBRARY_PATH
-nohup INSTALL_DIR/bin/UpdateManager --execute "STACKBUILDER_DIR/scripts/launchStackBuilder.sh" > /tmp/um_update_manager.log 2>&1 &
-rm -f /tmp/um_update_manager.log
-
