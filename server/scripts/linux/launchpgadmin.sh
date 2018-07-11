21e317cf9d91b4dc5644e0e16783170c81cb31f8
zilanbasha.shaik <zilanbasha.shaik@enterprisedb.com>
2018-07-11 12:10:23 +0530

    [pgAdmin,Stackbuilder]   - libpng causes the issue while launching the pgAdmin as it has dependecny on libz so used the LD_PRELOAD variable to launch it without any issues.   - same issue with Stackbuilder as well added the LD_PRELOAD variable to launch it without any issues.   - Reviewed by Sandeep Thakkar.

--- server/scripts/linux/launchpgadmin.sh
+++ server/scripts/linux/launchpgadmin.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 # Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
 
-LD_LIBRARY_PATH=PG_INSTALLDIR/pgAdmin3/lib:PG_INSTALLDIR/lib:$LD_LIBRARY_PATH G_SLICE=always-malloc PG_INSTALLDIR/pgAdmin3/bin/pgadmin3
+#Is it Ubuntu?
+uname -a | grep -i ubuntu > /dev/null
+if [ "$?" -eq "0" ]
+then
+    #assuming /etc/lsb-release is always present on Ubuntu
+    #libpng causes the issue while launching the pgAdmin as it has dependecny on libz so used the LD_PRELOAD variable to launch it without any issues.
+    UBUNTU_VERSION=`grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f2 | cut -d "." -f1`
+    if [ "$UBUNTU_VERSION" -ge "17" ];
+    then
+       SET_LDPRELOAD="LD_PRELOAD=/lib/x86_64-linux-gnu/libz.so.1"
 
+    fi
+fi
 
+  if [ ! -z "$SET_LDPRELOAD" ];
+  then
+     export eval ${SET_LDPRELOAD}
+  fi
+  LD_LIBRARY_PATH=PG_INSTALLDIR/pgAdmin3/lib:PG_INSTALLDIR/lib:$LD_LIBRARY_PATH G_SLICE=always-malloc PG_INSTALLDIR/pgAdmin3/bin/pgadmin3
--- server/scripts/linux/runstackbuilder.sh
+++ server/scripts/linux/runstackbuilder.sh
@@ -69,6 +69,24 @@ echo "No graphical su/sudo program could be found on your system!"
 echo "This window must be kept open while Stack Builder is running."
 echo ""
 
+#Is it Ubuntu?
+uname -a | grep -i ubuntu > /dev/null
+if [ "$?" -eq "0" ];
+then
+    #assuming /etc/lsb-release is always present on Ubuntu
+    #libpng causes the issue while launching the Stackbuilder as it has dependecny on libz so used the LD_PRELOAD variable to launch it without any issues.
+
+    UBUNTU_VERSION=`grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f2 | cut -d "." -f1`
+    if [ "$UBUNTU_VERSION" -ge "17" ];
+    then
+       SET_LDPRELOAD="LD_PRELOAD=/lib/x86_64-linux-gnu/libz.so.1"
+    fi
+fi
+if [ ! -z "$SET_LDPRELOAD" ];
+then
+   export eval ${SET_LDPRELOAD}
+fi
+
 # You're not running this script as root user
 if [ x"$LOADINGUSER" != x"root" ];
 then
