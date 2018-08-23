4cb514b80d2b1438b37116dbc3e2dddfbb39337c
Srinu Perabattula <srinu.perabattula@enterprisedb.com>
2018-08-23 11:27:46 +0530

    Update Server Build number to 2 for OpenSSL Update

--- versions.sh
+++ versions.sh
@@ -29,7 +29,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.4
-PG_MINOR_VERSION=19.1
+PG_MINOR_VERSION=19.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.25
