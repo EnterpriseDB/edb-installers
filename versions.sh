667e285061bd48517a0ca40c73676e02c9f42025
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2018-10-23 09:17:00 +0530

    Bump Server build number to provide an update on Windows because of vcredist issue

--- versions.sh
+++ versions.sh
@@ -36,7 +36,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=11
-PG_MINOR_VERSION=0.1
+PG_MINOR_VERSION=0.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.33
