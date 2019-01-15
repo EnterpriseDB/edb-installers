ccfa345618d052c3a149466ca9ede8c54d35ac18
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2019-01-15 11:44:56 +0530

    Update Copyright year in the installer scripts

--- server/scripts/linux/runpsql.sh
+++ server/scripts/linux/runpsql.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2019, EnterpriseDB Corporation.  All rights reserved
 
 # PostgreSQL psql runner script for Linux
 
