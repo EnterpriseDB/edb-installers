c54c31a26bd9f8a51872564aadd19896bf696582
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2019-01-15 11:41:28 +0530

    Update Copyright year in the installer scripts

--- server/scripts/solaris/launchpsql.sh
+++ server/scripts/solaris/launchpsql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012-2018, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2019, EnterpriseDB Corporation.  All rights reserved
 
 # PostgreSQL psql launch script for Linux
 
