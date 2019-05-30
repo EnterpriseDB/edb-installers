3e3d6e3bc810004d8bbcd7baca0246f489212353
zilanbasha.shaik <zilanbasha.shaik@enterprisedb.com>
2019-05-30 09:16:23 +0530

    [server] - Bump server build number to provide new updates because of pgAdmin4 updates. - Add Bitrock installer error messages to es.lng, fr.lng language files.

--- versions.sh
+++ versions.sh
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.6
-PG_MINOR_VERSION=13.2
+PG_MINOR_VERSION=13.3
 
 # Other package versions
 PG_VERSION_APACHE=2.4.39
