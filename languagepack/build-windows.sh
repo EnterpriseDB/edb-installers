6ec814c023a954601e4768c19041061c04c6dfe3
Talha Bin Rizwan <talha.rizwan@enterprisedb.com>
2019-09-12 16:22:26 +0500

    [CMBR-1342], Building Python-3.7 with our version of OpenSSL(currently 1.1.1c) for Win64.

--- languagepack/build-windows.sh
+++ languagepack/build-windows.sh
@@ -40,10 +40,10 @@ _prep_languagepack_windows() {
     extract_file $WD/../tarballs/tcl8.6.8-src || _die "Failed to extract tcl/tk source (tcl-8.6.8-src.tar.gz)"
     extract_file $WD/../tarballs/tk8.6.8-src || _die "Failed to extract tcl/tk source (tk-8.6.8-src.tar.gz)"
     extract_file $WD/../tarballs/perl-5.26.2 || _die "Failed to extract perl source (perl-5.26.2.tar.gz)"
-    extract_file $WD/../tarballs/Python-3.6.5 || _die "Failed to extract python source (Python-3.6.5.tgz)"
+    extract_file $WD/../tarballs/Python-3.7.4 || _die "Failed to extract python source (Python-3.7.4.tgz)"
     extract_file $WD/../tarballs/setuptools-39.2.0 || _die "Failed to extract python source (setuptools-39.2.0)"
 
-    ####cp ../../scripts/$ARCH/tix-8.4.3.4-VC12.patch Python-3.6.5 || _die "Failed to copy the tix build patch tix-8.4.3.4-VC12.patch"
+    ####cp ../../scripts/$ARCH/tix-8.4.3.4-VC12.patch Python-3.7.4 || _die "Failed to copy the tix build patch tix-8.4.3.4-VC12.patch"
 
     if [ "$ARCH" = "windows-x32" ];
     then
@@ -57,7 +57,7 @@ _prep_languagepack_windows() {
         sed -i '/^DEFINES\t\t= $(DEFINES) -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE/s/^/#/g' Makefile
 
         # Python related changes - x32
-        cd $WD/languagepack/source/languagepack.$ARCH/Python-3.6.5/PCbuild
+        cd $WD/languagepack/source/languagepack.$ARCH/Python-3.7.4/PCbuild
         sed -i '/{E5B04CC0-EB4C-42AB-B4DC-18EF95F864B0}.Release|Win32.Build.0/d' pcbuild.sln || _die "Failed to disable OpenSSL build which comes with Python"
         sed -i 's/liblzma.a/liblzma.lib/g' _lzma.vcxproj || _die "Failed to change liblzma.a to liblzma.lib in _lzma.vcxproj"
         sed -i 's/inc32/include/g;s/out32/lib/g' _hashlib.vcxproj || _die "Failed to change inc32 to include and out32 to lib for OpenSSL libs in _hashlib.vcxproj"
@@ -80,7 +80,7 @@ _prep_languagepack_windows() {
         ####sed -i '/^DEFINES\t\t= $(DEFINES) -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE/s/^/#/g' Makefile
 
         # Python related changes - x64
-        ####cd $WD/languagepack/source/languagepack.$ARCH/Python-3.6.5/PCbuild
+        ####cd $WD/languagepack/source/languagepack.$ARCH/Python-3.7.4/PCbuild
         ####sed -i '/{E5B04CC0-EB4C-42AB-B4DC-18EF95F864B0}.Release|x64.Build.0/d' pcbuild.sln || _die "Failed to disable OpenSSL build which comes with Python"
         ####sed -i 's/inc64/include/g;s/out64/lib/g' _hashlib.vcxproj || _die "Failed to change inc32 to include and out32 to lib for OpenSSL libs in _hashlib.vcxproj"
         ####sed -i 's/inc64/include/g;s/out64/lib/g' _ssl.vcxproj || _die "Failed to change inc32 to include and out32 to lib for OpenSSL libs in _ssl.vcxproj"
@@ -94,7 +94,7 @@ _prep_languagepack_windows() {
     fi
 
     #Python related changes - x32/x64
-    ####cd $WD/languagepack/source/languagepack.$ARCH/Python-3.6.5
+    ####cd $WD/languagepack/source/languagepack.$ARCH/Python-3.7.4
     ####sed -i "s|<opensslDir>\$(externalsDir).*|<opensslDir>${PG_PGBUILD_WIN}</opensslDir>|g" PCbuild/pyproject.props || _die "Failed to update pyproject.props"
     ####sed -i 's/#if _MSC_VER >= 1800/#if _MSC_VER > 1800/g' PC/pyconfig.h || _die "Failed to update pyconfig.h"
     ####sed -i "s/VS100COMNTOOLS/VS120COMNTOOLS/g" PCbuild/env.bat || _die "Failed to update env.bat"
@@ -197,23 +197,23 @@ EOT
     # Python Build
     cd $WD/languagepack/scripts/$ARCH
     ####scp Python_Build_Dependencies.bat $PG_SSH_WIN:$PG_PATH_WIN\\\\languagepack.$ARCH || _die "Failed to copy the Python_Build_Dependencies.bat to the windows build host"
-    ####ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5; cmd /c ..\\\\Python_Build_Dependencies.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5 $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5\\\\externals\\\\xz-5.0.5"
+    ####ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4; cmd /c ..\\\\Python_Build_Dependencies.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4 $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4\\\\externals\\\\xz-5.0.5"
 
     # Generating/Updating liblzma.def file for Python Build
     ####if [ "$ARCH" = "windows-x32" ];
     ####then
-    ####    scp $PG_SSH_WIN:$PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5\\\\externals\\\\xz-5.0.5\\\\bin_i486\\\\liblzma.def $WD/languagepack/scripts/$ARCH/liblzma.def || _die "Failed to get liblzma.def from windows build host"
+    ####    scp $PG_SSH_WIN:$PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4\\\\externals\\\\xz-5.0.5\\\\bin_i486\\\\liblzma.def $WD/languagepack/scripts/$ARCH/liblzma.def || _die "Failed to get liblzma.def from windows build host"
     ####    LinesBefore=$(grep -n "ordinal .*hint .*RVA .*name" liblzma.def | cut -d":" -f1)
     ####    sed -i "1,$(expr $LinesBefore)d" liblzma.def
     ####    TotalLines=$(grep -n "^[[:space:]]*Summary[[:space:]]*$" liblzma.def | cut -d":" -f1)
     ####    head -$(expr $TotalLines - 2) liblzma.def | awk -F" " '{print $4}' | sed '1 s/.*/EXPORTS/' > temp.def && mv temp.def liblzma.def
     ####    dos2unix liblzma.def
-    ####    scp liblzma.def $PG_SSH_WIN:$PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5\\\\externals\\\\xz-5.0.5\\\\bin_i486 || _die "Failed to copy liblzma.def to the windows build host"
+    ####    scp liblzma.def $PG_SSH_WIN:$PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4\\\\externals\\\\xz-5.0.5\\\\bin_i486 || _die "Failed to copy liblzma.def to the windows build host"
     ####fi
 
-    ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH; mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.6; cmd /c Python_Build.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5 $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.6 $PG_PATH_WIN\\\\languagepack.$ARCH $PG_PGBUILD_WIN BUILD"
-    ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH; mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.6; cmd /c Python_Build.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.6.5 $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.6 $PG_PATH_WIN\\\\languagepack.$ARCH $PG_PGBUILD_WIN INSTALL"
-    ####ssh $PG_SSH_WIN "sed -i 's/import winrandom/from Crypto.Random.OSRNG import winrandom/' $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.6\\\\Lib\\\\site-packages\\\\Crypto\\\\Random\\\\OSRNG\\\\nt.py"
+    ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH; mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.7; cmd /c Python_Build.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4 $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.7 $PG_PATH_WIN\\\\languagepack.$ARCH $PG_PGBUILD_WIN BUILD"
+    ssh $PG_SSH_WIN "cd $PG_PATH_WIN\\\\languagepack.$ARCH; mkdir -p $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.7; cmd /c Python_Build.bat $PG_PATH_WIN\\\\languagepack.$ARCH\\\\Python-3.7.4 $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.7 $PG_PATH_WIN\\\\languagepack.$ARCH $PG_PGBUILD_WIN INSTALL"
+    ####ssh $PG_SSH_WIN "sed -i 's/import winrandom/from Crypto.Random.OSRNG import winrandom/' $PG_LANGUAGEPACK_INSTALL_DIR_WIN\\\\Python-3.7\\\\Lib\\\\site-packages\\\\Crypto\\\\Random\\\\OSRNG\\\\nt.py"
 
     echo "Removing last successful staging directory ($PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging)"
     ssh $PG_SSH_WIN "cmd /c if EXIST $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging rd /S /Q $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging" || _die "Couldn't remove the last successful staging directory directory"
@@ -264,25 +264,25 @@ _postprocess_languagepack_windows() {
     mkdir -p $WD/languagepack/staging/$ARCH || _die "Couldn't create the staging directory"
     chmod ugo+w $WD/languagepack/staging/$ARCH || _die "Couldn't set the permissions on the staging directory"
 
-    ssh $PG_SSH_WIN "cd $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging; zip -r Tcl-8.6.zip Tcl-8.6; zip -r Perl-5.26.zip Perl-5.26; zip -r Python-3.6.zip Python-3.6" || _die "Failed to create Tcl-8.6.zip;Perl-5.26.zip;Python-3.6.zip on  windows buildhost"
+    ssh $PG_SSH_WIN "cd $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging; zip -r Tcl-8.6.zip Tcl-8.6; zip -r Perl-5.26.zip Perl-5.26; zip -r Python-3.7.zip Python-3.7" || _die "Failed to create Tcl-8.6.zip;Perl-5.26.zip;Python-3.7.zip on  windows buildhost"
     rsync -av $PG_SSH_WIN:$PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging/Tcl-8.6.zip  $WD/languagepack/staging/$ARCH || _die "Failed to copy Tcl-8.6.zip"
     rsync -av $PG_SSH_WIN:$PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging/Perl-5.26.zip  $WD/languagepack/staging/$ARCH || _die "Failed to copy Perl-5.26.zip"
-    rsync -av $PG_SSH_WIN:$PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging/Python-3.6.zip  $WD/languagepack/staging/$ARCH || _die "Failed to copy Python-3.6.zip"
+    rsync -av $PG_SSH_WIN:$PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging/Python-3.7.zip  $WD/languagepack/staging/$ARCH || _die "Failed to copy Python-3.7.zip"
     rsync -av $PG_SSH_WIN:$PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging/versions-${ARCH}.sh  $WD/languagepack/staging/$ARCH || _die "Failed to copy versions-${ARCH}.sh"
 
-    ssh $PG_SSH_WIN "cd $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging; rm -f Tcl-8.6.zip Perl-5.26.zip Python-3.6.zip " || _die "Failed to remove  Tcl-8.6.zip;Perl-5.26.zip; Python-3.6.zip on  windows buildhost"
+    ssh $PG_SSH_WIN "cd $PG_LANGUAGEPACK_INSTALL_DIR_WIN.staging; rm -f Tcl-8.6.zip Perl-5.26.zip Python-3.7.zip " || _die "Failed to remove  Tcl-8.6.zip;Perl-5.26.zip; Python-3.7.zip on  windows buildhost"
 
     cd $WD/languagepack/staging/$ARCH/
     unzip Tcl-8.6.zip ||_die "Failed to unzip Tcl-8.6.zip"
     unzip Perl-5.26.zip || _die "Failed to unzip Perl-5.26.zip"
-    unzip Python-3.6.zip || _die "Failed to unzip Python-3.6.zip"
-    rm -f Tcl-8.6.zip Perl-5.26.zip Python-3.6.zip || _die "Failed to remove the Tcl-8.6.zip;Perl-5.26.zip;Python-3.6.zip"
+    unzip Python-3.7.zip || _die "Failed to unzip Python-3.7.zip"
+    rm -f Tcl-8.6.zip Perl-5.26.zip Python-3.7.zip || _die "Failed to remove the Tcl-8.6.zip;Perl-5.26.zip;Python-3.7.zip"
 
     dos2unix $WD/languagepack/staging/$ARCH/versions-${ARCH}.sh || _die "Failed to convert format of versions-${ARCH}.sh from dos to unix"
     source $WD/languagepack/staging/$ARCH/versions-${ARCH}.sh
     PG_BUILD_LANGUAGEPACK=$(expr $PG_BUILD_LANGUAGEPACK + $SKIPBUILD)
 
-    ####mv $WD/languagepack/staging/$ARCH/Python-3.6/pip_packages_list.txt $WD/languagepack/staging/$ARCH || _die "Failed to move pip_packages_list.txt to $WD/languagepack/staging/$ARCH"
+    ####mv $WD/languagepack/staging/$ARCH/Python-3.7/pip_packages_list.txt $WD/languagepack/staging/$ARCH || _die "Failed to move pip_packages_list.txt to $WD/languagepack/staging/$ARCH"
 
     cd $WD/languagepack
     pushd staging/$ARCH
