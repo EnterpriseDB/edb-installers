a2ccf9211616efeca607a3ead4bc7a6ad031f478
zilanbasha.shaik <zilanbasha.shaik@enterprisedb.com>
2019-12-21 18:26:17 +0530

    - Server app bundle fails to initialize on 10.14+. Include the entitlements for the server to fix the issue. - Notarize the PostGIS macOS builds, Bump it's build number, updated settings.sh.in with new variables.

--- common.sh
+++ common.sh
@@ -399,7 +399,7 @@ sign_binaries()
                 #error: invalid or inappropriate API flag(s) specified
 
                 security unlock-keychain -p $KEYCHAIN_PASSWD ~/Library/Keychains/login.keychain
-                codesign --deep -f -i "com.edb.postgresql" -s "Developer ID Application" --options 0x10000 $i
+                codesign --deep -f -i "com.edb.postgresql" -s "Developer ID Application" --options runtime $i
            fi
 
 
@@ -412,6 +412,6 @@ sign_libraries()
 {
 
         security unlock-keychain -p $KEYCHAIN_PASSWD ~/Library/Keychains/login.keychain
-        find $1 -type f -name "*.dylib*" -exec codesign --deep -f -i "com.edb.postgresql" -s "Developer ID Application" --options 0x10000 {} \;
+        find $1 -type f -name "*.dylib*" -exec codesign --deep -f -i "com.edb.postgresql" -s "Developer ID Application" --options runtime {} \;
 
 }
