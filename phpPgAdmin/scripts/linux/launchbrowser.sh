a8972f07424bfebe1ddd206cfb3185877587b5ee
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2020-01-07 11:43:48 +0530

    Update copyright year in the scripts

--- phpPgAdmin/scripts/linux/launchbrowser.sh
+++ phpPgAdmin/scripts/linux/launchbrowser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-# Copyright (c) 2012-2-2019, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2-2020, EnterpriseDB Corporation.  All rights reserved
 ##################################################################################
 # launchbrowser.sh
 #
