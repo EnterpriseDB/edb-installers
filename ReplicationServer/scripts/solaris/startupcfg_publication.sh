cc3559b93fa5080ec714f193cc007e2cb7e1bb02
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2020-01-07 11:45:44 +0530

    Update copyright year in the scripts

--- ReplicationServer/scripts/solaris/startupcfg_publication.sh
+++ ReplicationServer/scripts/solaris/startupcfg_publication.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012-2019, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2020, EnterpriseDB Corporation.  All rights reserved
 
 # Check the command line
 if [ $# -ne 4 ]; 
