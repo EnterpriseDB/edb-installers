384a1cc242150081fcb6f28be7afd50f564f9db3
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2020-01-07 11:44:28 +0530

    Update copyright year in the scripts

--- server/scripts/osx/initcluster.sh
+++ server/scripts/osx/initcluster.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2012-2019, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2020, EnterpriseDB Corporation.  All rights reserved
 
 # PostgreSQL cluster init script for OSX
 
