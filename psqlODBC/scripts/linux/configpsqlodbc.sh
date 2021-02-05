8b3527f963393842fbe09cba10ad4de23f2eb908
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2021-02-05 10:20:56 +0530

    Update copyright year in the installer scripts

--- psqlODBC/scripts/linux/configpsqlodbc.sh
+++ psqlODBC/scripts/linux/configpsqlodbc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2012-2020, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2021, EnterpriseDB Corporation.  All rights reserved
 
 # Check the command line
 if [ $# -ne 1 ];
--- server/scripts/osx/startupcfg.sh
+++ server/scripts/osx/startupcfg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2012-2020, EnterpriseDB Corporation.  All rights reserved
+# Copyright (c) 2012-2021, EnterpriseDB Corporation.  All rights reserved
 
 # PostgreSQL server startup configuration script for OSX
 
