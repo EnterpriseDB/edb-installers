bd91d5878fd9b920f9177a396633a78a0a80ad36
Mihir Pandya <mihir.pandya@enterprisedb.com>
2021-04-01 14:39:05 +0530

    Bump the build number to provide the updates with bundled openssl 1.1.1k

--- versions.sh
+++ versions.sh
@@ -35,7 +35,7 @@ PG_TAG_MIGRATIONTOOLKIT=''
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=9.6
-PG_MINOR_VERSION=21.1
+PG_MINOR_VERSION=21.2
 
 # Other package versions
 PG_VERSION_APACHE=2.4.39
