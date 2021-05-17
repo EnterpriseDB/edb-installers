702408778eaf47ef83d82bf8c033be3f512f91fb
Mihir Pandya <mihir.pandya@enterprisedb.com>
2021-05-17 14:34:18 +0530

    Update the build script to bundle pgAdmin4.ico file and update pgadmin.xml.in to display the correct pgadmin4 icon for windows-x64 platform.

--- server/build-windows-x64.sh
+++ server/build-windows-x64.sh
@@ -728,6 +728,7 @@ _postprocess_server_windows_x64() {
    # Copy in the menu pick images and XDG items
     mkdir -p $PGADMIN_STAGING_WINDOWS_X64/scripts/images || _die "Failed to create a directory for the menu pick images"
     cp resources/pg-help.ico $PGADMIN_STAGING_WINDOWS_X64/scripts/images/pgadmin-help.ico || _die "Failed to copy the menu pick images (resources/pg-help.ico)"
+    cp $WD/server/source/pgadmin.windows-x64/pkg/win32/Resources/pgAdmin4.ico $PGADMIN_STAGING_WINDOWS_X64/scripts/images/ || _die "Failed to copy the pgAdmin4.ico file"
 
     # Copy the launch scripts
     cp scripts/windows/serverctl.vbs $PGSERVER_STAGING_WINDOWS_X64/scripts/serverctl.vbs || _die "Failed to copy the serverctl script (scripts/windows/serverctl.vbs)"
