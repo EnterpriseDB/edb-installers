8fe77ce21eb4d4f23761d277e10c2113b332d836
Semab-Tariq <Semab.Tariq@enterprisedb.com>
2021-07-28 13:19:40 +0500

    [PPS-284] Cleaned up PostGIS component

--- PostGIS/scripts/linux/check-pgversion.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012-2021, EnterpriseDB Corporation.  All rights reserved
-
-export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
-ver=`$1/bin/pg_config --version | cut -f2 -d " " | cut -f1,2 -d "."`
-echo $ver | sed -e 's:\([0-9].[0-9]\).*:\1:g'
--- PostGIS/scripts/linux/launchJDBCDocs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012-2021, EnterpriseDB Corporation.  All rights reserved
-
-INSTALLDIR="INSTALL_DIR"
-DOCDIR="DOC_DIR"
-URL="file://$DOCDIR/postgis/overview.html"
-
-"$INSTALLDIR/PostGIS/scripts/launchbrowser.sh" $URL
-
