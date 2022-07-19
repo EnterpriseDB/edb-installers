a7b9fcb660eaef90bdc88590a29a5995653874a0
Semab-Tariq <Semab.Tariq@enterprisedb.com>
2022-07-19 15:25:47 +0500

    [PPS-284] - Deleting build scripts of unsupported platfroms and Removing some components that are not required anymore

--- Slony/scripts/linux/launchSlonyDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012-2022, EnterpriseDB Corporation.  All rights reserved
-
-INSTALLDIR="INSTALL_DIR"
-URL="http://www.slony.info/documentation/"
-
-"$INSTALLDIR/Slony/scripts/launchbrowser.sh" $URL
-
