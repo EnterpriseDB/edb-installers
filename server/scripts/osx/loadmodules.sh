804077065fc1f91492e603b34bcbbce84bebdb7d
Manika Singhal <manikasinghal@MAC-C02G66TDMD6R.local>
2023-01-17 16:19:31 +0530

    [DBP-173]: Updated loadmodules script for su - occurrence

--- server/scripts/osx/loadmodules.sh
+++ server/scripts/osx/loadmodules.sh
@@ -41,7 +41,7 @@ chown $OSUSERNAME:daemon $INSTALLDIR/installer/server/pgpass.$$ || _die "Failed 
 
 # Install adminpack in the postgres database
 echo "Installing the adminpack module in the postgres database..."
-sudo -u $OSUSERNAME "PGPASSFILE=$INSTALLDIR/installer/server/pgpass.$$ $INSTALLDIR/bin/psql -U $SUPERUSERNAME -p $PORT -c \"CREATE EXTENSION adminpack;\" postgres" || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
+sudo -u $OSUSERNAME PGPASSFILE="$INSTALLDIR"/installer/server/pgpass.$$ "$INSTALLDIR"/bin/psql -U "$SUPERUSERNAME" -p "$PORT" -c "CREATE EXTENSION adminpack;" postgres || _warn "Failed to install the 'adminpack' module in the 'postgres' database"
 
 # Cleanup
 if [ -f $INSTALLDIR/installer/server/pgpass.$$ ];
