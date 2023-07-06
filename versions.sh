5e0d868cda2afa46f599c69018b65df5c55741d4
Manika Singhal <manikasinghal@MAC-C02G66TDMD6R.local>
2023-07-06 16:51:46 +0530

    [DBP-400]: Bump PG build number to 3

--- versions.sh
+++ versions.sh
@@ -21,7 +21,7 @@ PG_BUILDNUM_SQLPROTECT=2
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=12
-PG_MINOR_VERSION=15.2
+PG_MINOR_VERSION=15.3
 
 # Other package versions
 PG_VERSION_POSTGIS=2.5.5
