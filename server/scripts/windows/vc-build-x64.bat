e218721e9086018f4e769cf49493472f04fc998a
manika.singhal <manika.singhal@enterprisedb.com>
2024-07-16 12:36:03 +0530

    update bat file to collect all additional options

--- server/scripts/windows/vc-build-x64.bat
+++ server/scripts/windows/vc-build-x64.bat
@@ -1,16 +1,37 @@
+setlocal enabledelayedexpansion
 ECHO Setting variables
 
+rem Set fixed parameters from the command line arguments
 @SET PROJECT_FILE=%1
 @SET CONFIGURATION=%2
 @SET PLATFORM=%3
 @SET OUTDIR=%4
 @SET TOOLSET=%5
-@SET ADDITIONAL_OPTIONS=%6
+
+rem Shift the fixed parameters to process additional options
+shift
+shift
+shift
+shift
+shift
+
+rem Initialize ADDITIONAL_OPTIONS as an empty variable
+@SET ADDITIONAL_OPTIONS=
+
+rem Loop to collect all additional options into ADDITIONAL_OPTIONS
+:loop
+if "%1"=="" goto done
+set ADDITIONAL_OPTIONS=!ADDITIONAL_OPTIONS! %1
+shift
+goto loop
+
+:done
 
 IF "%CONFIGURATION%" == "UPGRADE" GOTO upgrade
 IF "%TOOLSET%" == "" ( SET TOOLSET=v143
 )
 
+rem Run msbuild with all parameters and additional options
 msbuild %PROJECT_FILE% /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% /p:OutDir=%OUTDIR% /p:PlatformToolset=%TOOLSET% %ADDITIONAL_OPTIONS%  || EXIT /B 1
 GOTO end
 
