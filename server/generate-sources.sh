d7874d1e9a6f0d27beca6b15d4ac97d491e0f49c
manika.singhal <manika.singhal@enterprisedb.com>
2024-07-26 19:47:14 +0530

    update files as per needs for PG 17beta versions

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -8,7 +8,7 @@ NAME=postgresql
 
 WORKDIR=$(pwd)/src
 cd ${WORKDIR}
-TARNAME="${NAME}-${VERSION}.${EXTRA_VERSION:-}"
+TARNAME="${NAME}-${VERSION}${EXTRA_VERSION:+.$EXTRA_VERSION}"
 wget ${URL}
 md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
 mv ${TARNAME}.tar.bz2 ../
