60bafd65198a42cb529236d721607bfe161e0bee
manika.singhal <manika.singhal@enterprisedb.com>
2024-07-26 20:39:45 +0530

    Adding bat script to support meson build in PG17
    DBP-858, Manika Singhal

--- server/scripts/windows/compile.ps1
+++ server/scripts/windows/compile.ps1
@@ -168,7 +168,7 @@ Set-Acl $temporary_data_location $Acl
 
 # So far, so good. Let's start compiling
 Write-Host "Executing meson bat file"
-Start-Process -FilePath packaging-config/installer/server/scripts/windows/meson.bat -Wait -NoNewWindow
+Start-Process -FilePath packaging-config/server/scripts/windows/meson.bat -Wait -NoNewWindow
 
 Write-Host "copying meson-install content to $installation_directory"
 Copy-Item -Path "$source_directory\meson-install\*" -Destination $installation_directory/ -Recurse
---
+++ server/scripts/windows/meson.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+CALL "C:\\Program Files\\Microsoft Visual Studio\\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+ECHO Creating build and install dirs...
+
+mkdir D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-build
+mkdir D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-install
+
+SET BASE_PATH=D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation
+
+SET PATH=%BASE_PATH%\pkg-config-lite-0.28-1\bin;C:\Strawberry\perl\bin;%BASE_PATH%\openssl\bin;%BASE_PATH%\zlib\bin;%BASE_PATH%\libxml2\bin;%BASE_PATH%\zstd\bin;%BASE_PATH%\1z4\bin;%BASE_PATH%\libxslt\bin;%BASE_PATH%\icu\bin;%PATH%
+
+SET PKG_CONFIG_PATH=%BASE_PATH%\zlib\lib\pkgconfig;%BASE_PATH%\libxml2\lib\pkgconfig;%BASE_PATH%\zstd\lib\pkgconfig;%BASE_PATH%\lz4\lib\pkgconfig;%BASE_PATH%\libxslt\lib\pkgconfig;%BASE_PATH%\icu\lib\pkgconfig;C:\Users\runneradmin\AppData\Local\Apps\Tcl86\lib\pkgconfig;%BASE_PATH%\uuid\lib\pkgconfig
+
+meson setup D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2 D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-build --prefix=D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-install -Dnls=enabled -Duuid=ossp -Dplperl=enabled -Dssl=openssl -Dextra_include_dirs=D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\gettext\include,D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\openssl\include -Dextra_lib_dirs=D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\gettext\lib,D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\openssl\lib
+
+cd D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-build
+ECHO meson build dir content
+dir
+ninja --verbose
+
+ninja install
+dir D:\a\postgresql-packaging-foundation\postgresql-packaging-foundation\postgresql-17beta2\meson-install
