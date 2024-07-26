f160f8f9def548f2db679ecb7da0fa3e4f372b97
Manika SInghal <69899716+Manika-EDB@users.noreply.github.com>
2024-07-26 07:53:16 +0100

    Make changes to windows build environment to support GitHub Actions
    Historically, the build-windows-x64.sh script was responsible to prepare the
    build environment, to compile the source code and to generate the BitRock
    based installer. This is now changed to using GitHub Actions based workflow
    and the supporting powershell scripts. The workflow resides in a separate
    repository whereas the build scripts and installer code resides in this
    repository.
    
    Summary of the Changes:
    - remove build-windows-x64.sh as it is no more needed, instead add
    new scripts compile.ps1, prepare-staging, compile-system-stats
    - packages-win64.txt contains 3rd party libs name and versions.
    - version.txt contains versions of PG, LP, stackbuilder and pgAdmin4
    - update the project files to suite the newer Visual Studio
    - additional fixes required in installer XML
    
    DBP-858, Manika Singhal, --Tested by Manika Singhal --Reviewed by Sandeep Thakkar,
    Srinu Perabattula and Kritika Agarwal

---
+++ server/scripts/windows/vc-build-x64.bat
@@ -0,0 +1,18 @@
+setlocal enabledelayedexpansion
+ECHO Setting variables
+
+rem Set fixed parameters from the command line arguments
+@SET PROJECT_FILE=%1
+@SET CONFIGURATION=%2
+@SET PLATFORM=%3
+@SET OUTDIR=%4
+@SET TOOLSET=%5
+
+rem Run msbuild with all parameters and additional options
+msbuild %PROJECT_FILE% /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM% /p:OutDir=%OUTDIR% /p:PlatformToolset=%TOOLSET% %~6 || EXIT /B 1
+GOTO end
+
+:upgrade
+devenv /upgrade %PROJECT_FILE%
+
+:end
