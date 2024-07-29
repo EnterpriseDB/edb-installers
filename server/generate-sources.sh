482500fc791daa7bfd73a7922b0548ca98238d98
manika.singhal <manika.singhal@enterprisedb.com>
2024-07-29 18:31:51 +0530

    modify the script to support all type of releases (beta, rc and ga)

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -4,11 +4,11 @@ set -xeu
 
 NAME=postgresql
 
-: ${VERSION:?The VERSION environment variable is required}
+: ${SOURCE_VERSION:?The SOURCE_VERSION environment variable is required}
 
 WORKDIR=$(pwd)/src
 cd ${WORKDIR}
-TARNAME="${NAME}-${VERSION}${EXTRA_VERSION:+.$EXTRA_VERSION}"
+TARNAME="${NAME}-${SOURCE_VERSION}"
 wget ${URL}
 md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
 mv ${TARNAME}.tar.bz2 ../
