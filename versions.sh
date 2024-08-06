32f70a417d841a9c4b240a8d0cdadafeb2bd3ab6
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2024-08-06 22:21:08 +0530

    [RL-182]: Bump PG version to 17beta3

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=17beta2
+PG_TARBALL_POSTGRESQL=17beta3
 PG_TARBALL_PGADMIN=8.10
 PG_LP_VERSION=4.3
 
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=17
-PG_MINOR_VERSION=0.beta2
+PG_MINOR_VERSION=0.beta3
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.2
