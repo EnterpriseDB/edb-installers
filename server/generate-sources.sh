0415e6462ddaccb6831a00987f1a59740c7ddce8
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2024-10-29 11:04:41 +0530

    Update generate-sources.sh

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -10,5 +10,5 @@ WORKDIR=$(pwd)/src
 cd ${WORKDIR}
 TARNAME="${NAME}-${VERSION}.${EXTRA_VERSION:-}"
 wget ${URL}
-md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
-mv ${TARNAME}.tar.bz2 ../
+md5sum "postgresql-15-snapshot.tar.bz2" > "${TARNAME}.tar.bz2.md5"
+mv postgresql-15-snapshot.tar.bz2 ../
