781182e9095ee668b44d190e09ce55020d883b9e
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2024-11-20 14:08:53 +0530

    Bump PG version to v12.22

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=12.21
+PG_TARBALL_POSTGRESQL=12.22
 PG_TARBALL_PGADMIN=8.13
 
 # Build nums
@@ -14,7 +14,7 @@ PG_BUILDNUM_SQLPROTECT=2
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=12
-PG_MINOR_VERSION=21.1
+PG_MINOR_VERSION=22.1
 
 # Other package versions
 PG_VERSION_POSTGIS=2.5.5
