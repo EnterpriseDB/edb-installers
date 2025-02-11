faf918b66bdec3597d0021ceb98e7b7604831094
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-02-11 21:39:08 +0530

    Bump PG version to v16.7
    DBP-1167 Kritika Agarwal

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=16.6
+PG_TARBALL_POSTGRESQL=16.7
 PG_TARBALL_PGADMIN=9.0
 PG_LP_VERSION=4.5
 
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=16
-PG_MINOR_VERSION=6.1
+PG_MINOR_VERSION=7.1
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.3
