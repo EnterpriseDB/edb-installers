48a48156f938770786833c8d0a179f584d814ed7
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-03-20 11:52:12 +0530

    Bump PG revision number to 2
    DBP-1304 Kritika Agarwal

--- versions.sh
+++ versions.sh
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=16
-PG_MINOR_VERSION=8.1
+PG_MINOR_VERSION=8.2
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.3
