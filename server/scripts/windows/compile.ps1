2164620b122aa6cd32251d9c11a381641778a15c
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-06-03 14:22:38 +0530

    Update compile.ps1

--- server/scripts/windows/compile.ps1
+++ server/scripts/windows/compile.ps1
@@ -194,6 +194,7 @@ Copy-Item $openssl_directory/bin/*.dll $installation_directory\bin
 Copy-Item $xml_directory/bin/*.dll $installation_directory\bin
 Copy-Item $xslt_directory/bin/libxslt.dll $installation_directory\bin
 Copy-Item $iconv_directory/bin/libiconv-2.dll $installation_directory\bin
+Copy-Item ./libwinpthread-1.dll $installation_directory\bin
 #Copy-Item $gettext_directory/bin/libwinpthread-1.dll $installation_directory\bin
 Copy-Item $zlib_directory/bin/*.dll $installation_directory\bin
 Copy-Item $zstd_directory/bin/*.dll $installation_directory\bin
