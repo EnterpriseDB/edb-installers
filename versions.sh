f30b9615b03051aabddb0dfb0b7109fae72bb55f
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-08-12 15:27:22 +0530

    Bump PG version to v14.19 (#376)

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=14.18
+PG_TARBALL_POSTGRESQL=14.19
 PG_TARBALL_PGADMIN=9.6
 PG_LP_VERSION=2.9
 
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=14
-PG_MINOR_VERSION=18.1
+PG_MINOR_VERSION=19.1
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.4
