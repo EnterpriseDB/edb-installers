665deeb9164c908732e520220d3368be9c379288
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-10-08 16:19:13 +0530

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -21,7 +21,7 @@ PG_BUILDNUM_SQLPROTECT=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=13
-PG_MINOR_VERSION=22.1
+PG_MINOR_VERSION=22.2
 
 # Other package versions
 PG_VERSION_PGJDBC=42.2.16
