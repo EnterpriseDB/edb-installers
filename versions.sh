8d7bc149b18a121c37b60615d83eb9435d06ac0d
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-10-08 16:57:02 +0530

    Bump PG revision number to 2 (#420)
    Update curl, xml, expat, pcre2 and openssl versions

--- versions.sh
+++ versions.sh
@@ -20,7 +20,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=15
-PG_MINOR_VERSION=14.1
+PG_MINOR_VERSION=14.2
 
 # Other package versions
 PG_VERSION_PGJDBC=42.7.2
