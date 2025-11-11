d00b6464a6ccda0a09fc67ffe492826a4f937583
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-11-11 18:36:47 +0530

    Bump PostgreSQL version to 14.20-1
    PgAdmin4 to v9.9
    
    Kritika Agarwal
    
    DBP-1688

--- versions.sh
+++ versions.sh
@@ -2,7 +2,7 @@
 
 # Source tarball versions
 
-PG_TARBALL_POSTGRESQL=14.19
+PG_TARBALL_POSTGRESQL=14.20
 PG_TARBALL_PGADMIN=9.9
 PG_LP_VERSION=2.9
 
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=14
-PG_MINOR_VERSION=19.2
+PG_MINOR_VERSION=20.1
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.4
