2a894ea771e53f41f222e2525c3f0f356952070a
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2025-12-11 13:24:12 +0530

    Bump PostgreSQL version to 17.7-2 (#457)

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 # Source tarball versions
 
 PG_TARBALL_POSTGRESQL=17.7
-PG_TARBALL_PGADMIN=9.9
+PG_TARBALL_PGADMIN=9.10
 PG_LP_VERSION=5.5
 
 # Build nums
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=17
-PG_MINOR_VERSION=7.1
+PG_MINOR_VERSION=7.2
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.4
