b58b74d5d102234a3b536f7966e8aa2b89e77338
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-02-10 14:59:33 +0530

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=17
-PG_MINOR_VERSION=8.3
+PG_MINOR_VERSION=8.1
 
 # Other package versions
 PG_VERSION_POSTGIS=3.4.4
