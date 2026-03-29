b3aee77d782b60c07d4b87a30161032f22b9f8d1
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-03-29 21:19:40 +0530

    Update meson.bat

--- server/scripts/windows/meson.bat
+++ server/scripts/windows/meson.bat
@@ -21,8 +21,7 @@ ECHO meson build dir content
 dir
 ninja --verbose
 
-ninja src/interfaces/ecpg/test/compat_informix/dec_test.exe
-ninja src/interfaces/ecpg/test/compat_informix/dec_test.exe
+ninja src/interfaces/ecpg/test/all
 
 ninja install
 dir %BASE_PATH%\%SOURCE_DIR%\meson-install
