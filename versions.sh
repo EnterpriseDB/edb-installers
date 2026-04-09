c33890deed2701456f2e773acdbf6737828a19fc
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-04-09 16:05:36 +0530

    Bump PG version to 18.3-3 (windows) & 18.3-4 (macOS)

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 # Source tarball versions
 
 PG_TARBALL_POSTGRESQL=18.3
-PG_TARBALL_PGADMIN=9.13
+PG_TARBALL_PGADMIN=9.14
 PG_LP_VERSION=6.2
 
 # Build nums
@@ -16,7 +16,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=18
-PG_MINOR_VERSION=3.3
+PG_MINOR_VERSION=3.4
 
 # Other package versions
 PG_VERSION_POSTGIS=3.6.0
