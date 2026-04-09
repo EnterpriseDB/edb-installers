79278bae9f814890d1a4430828cdea81f8641180
agarwal-kritika <62944221+agarwal-kritika@users.noreply.github.com>
2026-04-09 13:11:13 +0530

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -3,7 +3,7 @@
 # Source tarball versions
 
 PG_TARBALL_POSTGRESQL=15.17
-PG_TARBALL_PGADMIN=9.12
+PG_TARBALL_PGADMIN=9.14
 PG_LP_VERSION=3.9
 
 # Build nums
@@ -20,7 +20,7 @@ PG_BUILDNUM_LANGUAGEPACK=1
 #                     Minor version is revision.build.
 
 PG_MAJOR_VERSION=15
-PG_MINOR_VERSION=17.2
+PG_MINOR_VERSION=17.3
 
 # Other package versions
 PG_VERSION_PGJDBC=42.7.2
