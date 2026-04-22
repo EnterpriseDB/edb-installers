4df7c4a57b9d2ec0e03e0aab6ef34f70d5dcb99e
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-04-22 09:31:06 +0530

    Update prepare-staging-directory.sh

--- server/scripts/windows/prepare-staging-directory.sh
+++ server/scripts/windows/prepare-staging-directory.sh
@@ -92,6 +92,6 @@ cp -r ./curl/bin/libcurl.dll packaging-config/installer/server/staging/windows-x
 cp -r $(PWD)/SB/share packaging-config/installer/server/staging/windows-x64/stackbuilder
 
 #pgAdmin4
-mkdir -p "packaging-config/installer/server/staging/windows-x64"
+mkdir -p "packaging-config/installer/server/staging/windows-x64/pgAdmin"
 cp $(PWD)/pgAdmin4-binaries/pgadmin4-*.exe \
-   "packaging-config/installer/server/staging/windows-x64/"
+   "packaging-config/installer/server/staging/windows-x64/pgAdmin/"
