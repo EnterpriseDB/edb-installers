62d87aa81ae615b9e0c60a8b9c1e74f27ade506f
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-04-24 07:56:07 +0530

    added install-pgadmin.bat file

--- server/scripts/windows/install-pgadmin.bat
+++ server/scripts/windows/install-pgadmin.bat
@@ -1,6 +1,6 @@
 @echo off
 for /f "usebackq tokens=1" %%v in (`powershell -Command "(Invoke-WebRequest -Uri \"https://www.postgresql.org/ftp/pgadmin/pgadmin4/\" -UseBasicParsing).Links.href | Where-Object { $_ -match \"v[\d.]+\" } | ForEach-Object { $_ -replace \"v\",\"\" -replace \"/\",\"\" } | Sort-Object { [Version]$_ } -Descending | Select-Object -First 1"`) do (
-    curl -L "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v%%v/windows/pgadmin4-%%v-x64.exe" -o "%TEMP%\pgadmin4-latest-x64.exe"
-    "%TEMP%\pgadmin4-latest-x64.exe" /SILENT /SUPPRESSMSGBOXES /allusers
-    del "%TEMP%\pgadmin4-latest-x64.exe"
+    curl -L "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v%%v/windows/pgadmin4-%%v-x64.exe" -o "%TEMP%\pgadmin4-%%v-x64.exe"
+    "%TEMP%\pgadmin4-%%v-x64.exe" /SILENT /SUPPRESSMSGBOXES /allusers
+    del "%TEMP%\pgadmin4-%%v-x64.exe"
 )
