fe5c78674224aad44b70c3c950593630c3e5a5ff
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-04-27 11:14:36 +0530

    Update generate-sources.sh

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -4,5 +4,11 @@ NAME=postgresql
 : ${VERSION:?The VERSION environment variable is required}
 TARNAME="${NAME}-${VERSION}.${EXTRA_VERSION:-}"
 mv postgresql-source "${TARNAME}"
+
+# Run configure on Linux to generate pg_config.h for MSVC compatibility
+cd "${TARNAME}"
+./configure
+cd ..
+
 tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}"
 rm -rf "${TARNAME}"
