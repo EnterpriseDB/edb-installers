ba77f732a7314c26cbaa769360b8536e62c2e06b
Sandeep Thakkar <sandeep.thakkar@enterprisedb.com>
2026-04-28 13:17:57 +0530

    Improvise the handling of KEYCHAIN_PASSWD - Add KEYCHAIN_PASSWD secret credentials on Jenkins server - Add KEYCHAIN_PASSWD environment for individual jobs - Modify build scripts to pass the variable to the downstream scripts - Modify autobuild script to get rid of settings.sh.full
    DBP-1570, Reviewed by Kritika Agarwal

--- autobuild.sh
+++ autobuild.sh
@@ -112,9 +112,7 @@ git reset --hard >> autobuild.log 2>&1
 git checkout REL-14 >> autobuild.log 2>&1
 
 # Make sure, we always do a full build
-if [ -f settings.sh.full.REL-14 ]; then
-   cp -f settings.sh.full.REL-14 settings.sh
-fi
+cp -f settings.sh.in settings.sh
 
 # Self update
 echo "Updating REL-14 branch build system" >> autobuild.log
