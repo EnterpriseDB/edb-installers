db4bbd807146fadd0533ee77641237c0c0909969
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-06-18 21:18:36 +0530

    Update generate-sources.sh

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -2,12 +2,10 @@
 set -xeu
 NAME=postgresql
 : ${SOURCE_VERSION:?The SOURCE_VERSION environment variable is required}
-WORKDIR=$(pwd)
+WORKDIR=$(pwd)                             # ✅ workspace root
 TARNAME="${NAME}-${SOURCE_VERSION}"
 cd ${WORKDIR}
-
-# Rename src/ to postgresql-18.4/ so compile.ps1 finds the right directory
-cp -r src/ "${TARNAME}"                         # ✅ copy src to postgresql-18.4/
-tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/"    # ✅ pack as postgresql-18.4/
+cp -r src/ "${TARNAME}"                   # ✅ copy src/ to postgresql-18.4/
+tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/"  # ✅ pack as postgresql-18.4/
 md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
-rm -rf "${TARNAME}"                             # cleanup temp dir
+rm -rf "${TARNAME}"                        # ✅ cleanup temp dir
