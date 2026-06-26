7cd07adc970e7ecf3bc870b7497fbc1e30d68c5d
agarwal-kritika <kritika.agarwal@enterprisedb.com>
2026-06-26 11:19:04 +0530

    Updating..

--- server/scripts/osx/prepare-staging-directory.sh
+++ server/scripts/osx/prepare-staging-directory.sh
@@ -65,7 +65,6 @@ cp "$OSX_SCRIPTS/createshortcuts.sh"      "$PGSERVER/installer/server/createshor
 cp "$OSX_SCRIPTS/createshortcuts_server.sh" "$PGSERVER/installer/server/createshortcuts_server.sh"
 cp "$OSX_SCRIPTS/loadmodules.sh"          "$PGSERVER/installer/server/loadmodules.sh"
 cp "$OSX_SCRIPTS/install-pgadmin.sh"      "$PGSERVER/installer/server/install-pgadmin.sh"
-cp "$OSX_SCRIPTS/set-service-pl-env.sh"   "$PGSERVER/installer/server/set-service-pl-env.sh"
 chmod ugo+x "$PGSERVER/installer/prerun_checks.sh" "$PGSERVER/installer/server/"*.sh
 
 # Server menu-pick scripts (applescripts) and icons
