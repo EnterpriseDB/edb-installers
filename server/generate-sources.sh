5d0062ba1fe573c72d21902fcdec5ed97146c586
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-07-01 13:06:11 +0530

    Fix script to handle tarball creation and download

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -8,12 +8,19 @@ cd ${WORKDIR}
 
 if [ -n "${URL:-}" ]; then
   # release case: download tarball from URL
+  echo "Downloading source tarball from: ${URL}"
   wget -O "${TARNAME}.tar.bz2" ${URL}
+  echo "Tarball downloaded: ${TARNAME}.tar.bz2"
+  echo "Tarball md5sum: $(md5sum ${TARNAME}.tar.bz2)"
   md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
 else
   # snapshot case: create tarball from git checkout
+  echo "Source commit hash: $(git -C src rev-parse HEAD)"
+  echo "Source branch/ref: $(git -C src rev-parse --abbrev-ref HEAD)"
   cp -r src/ "${TARNAME}"
   tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/"
+  echo "Tarball created: ${TARNAME}.tar.bz2"
+  echo "Tarball md5sum: $(md5sum ${TARNAME}.tar.bz2)"
   md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
   rm -rf "${TARNAME}"
 fi
