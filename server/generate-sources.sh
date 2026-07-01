281877c69cc80cb821f6e5ca85c9f5db557e1cec
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-07-01 13:14:41 +0530

    Update generate-sources.sh

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -2,10 +2,25 @@
 set -xeu
 NAME=postgresql
 : ${SOURCE_VERSION:?The SOURCE_VERSION environment variable is required}
-WORKDIR=$(pwd)                      
+WORKDIR=$(pwd)
 TARNAME="${NAME}-${SOURCE_VERSION}"
 cd ${WORKDIR}
-cp -r src/ "${TARNAME}"      
-tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/" 
-md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
-rm -rf "${TARNAME}"
+
+if [ -n "${URL:-}" ]; then
+  # release case: download tarball from URL
+  echo "Downloading source tarball from: ${URL}"
+  wget -O "${TARNAME}.tar.bz2" ${URL}
+  echo "Tarball downloaded: ${TARNAME}.tar.bz2"
+  echo "Tarball md5sum: $(md5sum ${TARNAME}.tar.bz2)"
+  md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
+else
+  # snapshot case: create tarball from git checkout
+  echo "Source commit hash: $(git -C src rev-parse HEAD)"
+  echo "Source branch/ref: $(git -C src rev-parse --abbrev-ref HEAD)"
+  cp -r src/ "${TARNAME}"
+  tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/"
+  echo "Tarball created: ${TARNAME}.tar.bz2"
+  echo "Tarball md5sum: $(md5sum ${TARNAME}.tar.bz2)"
+  md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
+  rm -rf "${TARNAME}"
+fi
