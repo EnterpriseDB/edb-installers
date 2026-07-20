f4f706fb85c2b1d5b9cfe1800a7d95dda2faf69c
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-07-20 09:35:49 +0530

    Update generate-sources.sh

--- server/generate-sources.sh
+++ server/generate-sources.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
-set -xeu
+set -xe
 NAME=postgresql
 : ${VERSION:?The VERSION environment variable is required}
+: ${EXTRA_VERSION:?The EXTRA_VERSION environment variable is required}
+
+CONF_ARGS="--without-icu --without-readline"
 WORKDIR=$(pwd)
-TARNAME="${NAME}-${VERSION}"
-cd ${WORKDIR}
+TARNAME="${NAME}-${VERSION}.${EXTRA_VERSION}"
 
 if [ -n "${URL:-}" ]; then
   # release case: download tarball from URL
@@ -14,9 +16,20 @@ if [ -n "${URL:-}" ]; then
   echo "Tarball md5sum: $(md5sum ${TARNAME}.tar.bz2)"
   md5sum "${TARNAME}.tar.bz2" > "${TARNAME}.tar.bz2.md5"
 else
-  # snapshot case: create tarball from git checkout
+  # snapshot case: build docs and create tarball
   echo "Source commit hash: $(git -C src rev-parse HEAD)"
   echo "Source branch/ref: $(git -C src rev-parse --abbrev-ref HEAD)"
+
+  # install doc build dependencies
+  sudo apt-get install -y docbook-xsl docbook-xml xsltproc libxml2-utils
+
+  # build HTML docs
+  cd src
+  ./configure $CONF_ARGS
+  make -C doc/src/sgml html
+  cd ${WORKDIR}
+
+  # create tarball
   cp -r src/ "${TARNAME}"
   tar -cjf "${TARNAME}.tar.bz2" "${TARNAME}/"
   echo "Tarball created: ${TARNAME}.tar.bz2"
