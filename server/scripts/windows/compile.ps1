1cbaaa4ce885f0a5365c001ef1c1fc750da3440e
mayankjaiswal-dev <mayank.jaiswal@enterprisedb.com>
2026-09-22 11:24:59 +0530

    Update compile.ps1

--- server/scripts/windows/compile.ps1
+++ server/scripts/windows/compile.ps1
@@ -193,14 +193,14 @@ Get-ChildItem -Path "$installation_directory"
 # build directory to the installation directory, since
 # this file is needed to compile plpgsql_check
 #Copy-Item $source_directory/Release/plpgsql/plpgsql.lib $installation_directory/lib
-Copy-Item $gettext_directory/bin/libintl-9.dll $installation_directory\bin
+Copy-Item $gettext_directory/bin/libintl-8.dll $installation_directory\bin
 Copy-Item $icu_directory/bin/*.dll $installation_directory\bin
 Copy-Item $openssl_directory/bin/*.dll $installation_directory\bin
 Copy-Item $xml_directory/bin/*.dll $installation_directory\bin
 Copy-Item $xslt_directory/bin/libxslt.dll $installation_directory\bin
 Copy-Item $iconv_directory/bin/libiconv-2.dll $installation_directory\bin
 #Copy-Item $gettext_directory/bin/libiconv-2.dll $installation_directory\bin
-Copy-Item $gettext_directory/bin/libwinpthread-1.dll $installation_directory\bin
+#Copy-Item $gettext_directory/bin/libwinpthread-1.dll $installation_directory\bin
 Copy-Item $zlib_directory/bin/*.dll $installation_directory\bin
 Copy-Item $zstd_directory/bin/*.dll $installation_directory\bin
 Copy-Item $lz4_directory/bin/*.dll $installation_directory\bin
